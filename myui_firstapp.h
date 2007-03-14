c53157048c35d4386568d056aa41be11cc9d97b9
Hans Essel <H.Essel@gsi.de>
2007-03-14 12:00:07 +0000

    init
    git-svn-id: https://subversion.gsi.de/dabc/trunk@3 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

---
+++ myui_firstapp.h
@@ -0,0 +1,28 @@
+//
+// C++ Interface: myui_firstapp
+//
+// Description: 
+//
+//
+// Author: goofy <goofy@depc225>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef MYUI_FIRSTAPP_H
+#define MYUI_FIRSTAPP_H
+
+#include <myUi_First.h>
+
+/**
+	@author goofy <goofy@depc225>
+*/
+class myUi_FirstApp : public myUi_First
+{
+public:
+    myUi_FirstApp(QMainWindow* top=0);
+
+
+};
+
+#endif
