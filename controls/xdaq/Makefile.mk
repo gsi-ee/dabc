9fd4fdcea888bfda63a9af5a17e9b77e23041778
Joern Adamczewski <j.adamczewski@gsi.de>
2008-11-04 16:20:04 +0000

    git-svn-id: https://subversion.gsi.de/dabc/trunk@46 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- controls/xdaq/Makefile.mk
+++ controls/xdaq/Makefile.mk
@@ -5,7 +5,7 @@ libs::
 	cd $(DABCSYS)/controls/xdaq/dabc; $(MAKE)
 
 clean::
-	cd $(DABCSYS)/controls/xdaq/dabc/nodecontrol ; rm -rf lib; rm -rf src/*/*.o; rm -rf src/*/*.d
+	cd $(DABCSYS)/controls/xdaq/dabc/nodecontrol ; rm -rf lib; rm -rf src/*/*/*.o; rm -rf src/*/*/*.d
 	
 
 else
