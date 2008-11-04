d0fde5a941363acf944ab0fd4b9211d1969acd76
Joern Adamczewski <j.adamczewski@gsi.de>
2008-11-04 16:36:19 +0000

    install xdaq libs and includes to DABCSYS/lib and DABCSYS/include
    git-svn-id: https://subversion.gsi.de/dabc/trunk@48 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- controls/xdaq/Makefile.mk
+++ controls/xdaq/Makefile.mk
@@ -2,7 +2,7 @@ ifdef XDAQ_ROOT
 
 
 libs::
-	cd $(DABCSYS)/controls/xdaq/dabc; $(MAKE)
+	cd $(DABCSYS)/controls/xdaq/dabc; $(MAKE); $(MAKE) install
 
 clean::
 	cd $(DABCSYS)/controls/xdaq/dabc/nodecontrol ; rm -rf lib; rm -rf src/*/*/*.o; rm -rf src/*/*/*.d
