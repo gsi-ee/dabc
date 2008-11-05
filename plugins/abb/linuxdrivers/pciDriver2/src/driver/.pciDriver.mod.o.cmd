ba677922ec736a00bc5a4bd03d3b1155773a1f09
Sergey Linev <S.Linev@gsi.de>
2008-11-05 17:04:39 +0000

    Cleanup pcidriver repository
    git-svn-id: https://subversion.gsi.de/dabc/trunk@68 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

---
+++ plugins/abb/linuxdrivers/pciDriver2/src/driver/.pciDriver.mod.o.cmd
@@ -0,0 +1,209 @@
+cmd_/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.o := gcc -Wp,-MD,/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/.pciDriver.mod.o.d  -nostdinc -isystem /usr/lib64/gcc/x86_64-suse-linux/4.1.2/include -D__KERNEL__ -Iinclude  -include include/linux/autoconf.h -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Os  -march=k8 -m64 -mno-red-zone -mcmodel=kernel -pipe -fno-reorder-blocks -Wno-sign-compare -fno-asynchronous-unwind-tables -funit-at-a-time -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -maccumulate-outgoing-args -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -fomit-frame-pointer  -fno-stack-protector -Wdeclaration-after-statement -Wno-pointer-sign    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(pciDriver.mod)"  -D"KBUILD_MODNAME=KBUILD_STR(pciDriver)" -DMODULE -c -o /u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.o /u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.c
+
+deps_/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.o := \
+  /u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.c \
+    $(wildcard include/config/module/unload.h) \
+  include/linux/module.h \
+    $(wildcard include/config/modules.h) \
+    $(wildcard include/config/modversions.h) \
+    $(wildcard include/config/unused/symbols.h) \
+    $(wildcard include/config/generic/bug.h) \
+    $(wildcard include/config/kallsyms.h) \
+  include/linux/spinlock.h \
+    $(wildcard include/config/smp.h) \
+    $(wildcard include/config/debug/spinlock.h) \
+    $(wildcard include/config/preempt.h) \
+    $(wildcard include/config/debug/lock/alloc.h) \
+  include/linux/preempt.h \
+    $(wildcard include/config/debug/preempt.h) \
+  include/linux/thread_info.h \
+  include/linux/bitops.h \
+  include/asm/types.h \
+  include/asm/bitops.h \
+  include/asm/alternative.h \
+    $(wildcard include/config/paravirt.h) \
+  include/linux/types.h \
+    $(wildcard include/config/uid16.h) \
+    $(wildcard include/config/lbd.h) \
+    $(wildcard include/config/lsf.h) \
+    $(wildcard include/config/resources/64bit.h) \
+  include/linux/posix_types.h \
+  include/linux/stddef.h \
+  include/linux/compiler.h \
+    $(wildcard include/config/enable/must/check.h) \
+  include/linux/compiler-gcc4.h \
+    $(wildcard include/config/forced/inlining.h) \
+  include/linux/compiler-gcc.h \
+  include/asm/posix_types.h \
+  include/asm/cpufeature.h \
+  include/asm-generic/bitops/sched.h \
+  include/asm-generic/bitops/hweight.h \
+  include/asm-generic/bitops/ext2-non-atomic.h \
+  include/asm-generic/bitops/le.h \
+  include/asm/byteorder.h \
+  include/linux/byteorder/little_endian.h \
+  include/linux/byteorder/swab.h \
+  include/linux/byteorder/generic.h \
+  include/asm-generic/bitops/minix.h \
+  include/asm/thread_info.h \
+    $(wildcard include/config/debug/stack/usage.h) \
+  include/asm/page.h \
+    $(wildcard include/config/physical/start.h) \
+    $(wildcard include/config/flatmem.h) \
+  include/asm/bug.h \
+    $(wildcard include/config/bug.h) \
+    $(wildcard include/config/debug/bugverbose.h) \
+  include/asm-generic/bug.h \
+  include/asm-generic/memory_model.h \
+    $(wildcard include/config/discontigmem.h) \
+    $(wildcard include/config/sparsemem.h) \
+    $(wildcard include/config/out/of/line/pfn/to/page.h) \
+  include/asm-generic/page.h \
+  include/linux/log2.h \
+    $(wildcard include/config/arch/has/ilog2/u32.h) \
+    $(wildcard include/config/arch/has/ilog2/u64.h) \
+  include/asm/pda.h \
+    $(wildcard include/config/cc/stackprotector.h) \
+  include/linux/cache.h \
+  include/linux/kernel.h \
+    $(wildcard include/config/preempt/voluntary.h) \
+    $(wildcard include/config/debug/spinlock/sleep.h) \
+    $(wildcard include/config/printk.h) \
+    $(wildcard include/config/numa.h) \
+  /usr/lib64/gcc/x86_64-suse-linux/4.1.2/include/stdarg.h \
+  include/linux/linkage.h \
+  include/asm/linkage.h \
+  include/asm/cache.h \
+    $(wildcard include/config/x86/l1/cache/shift.h) \
+    $(wildcard include/config/x86/vsmp.h) \
+  include/asm/mmsegment.h \
+  include/linux/stringify.h \
+  include/linux/bottom_half.h \
+  include/asm/system.h \
+    $(wildcard include/config/unordered/io.h) \
+  include/asm/segment.h \
+  include/linux/irqflags.h \
+    $(wildcard include/config/trace/irqflags.h) \
+    $(wildcard include/config/trace/irqflags/support.h) \
+    $(wildcard include/config/x86.h) \
+  include/asm/irqflags.h \
+  include/linux/spinlock_types.h \
+  include/linux/lockdep.h \
+    $(wildcard include/config/lockdep.h) \
+    $(wildcard include/config/generic/hardirqs.h) \
+    $(wildcard include/config/prove/locking.h) \
+  include/asm/spinlock_types.h \
+  include/asm/spinlock.h \
+  include/asm/atomic.h \
+  include/asm-generic/atomic.h \
+  include/asm/rwlock.h \
+  include/asm/processor.h \
+  include/asm/sigcontext.h \
+  include/linux/threads.h \
+    $(wildcard include/config/nr/cpus.h) \
+    $(wildcard include/config/base/small.h) \
+  include/asm/msr.h \
+  include/asm/current.h \
+  include/asm/percpu.h \
+  include/linux/personality.h \
+  include/linux/cpumask.h \
+    $(wildcard include/config/hotplug/cpu.h) \
+  include/linux/bitmap.h \
+  include/linux/string.h \
+  include/asm/string.h \
+  include/linux/spinlock_api_smp.h \
+  include/linux/list.h \
+    $(wildcard include/config/debug/list.h) \
+  include/linux/poison.h \
+  include/linux/prefetch.h \
+  include/linux/stat.h \
+  include/asm/stat.h \
+  include/linux/time.h \
+  include/linux/seqlock.h \
+  include/linux/kmod.h \
+    $(wildcard include/config/kmod.h) \
+  include/linux/errno.h \
+  include/asm/errno.h \
+  include/asm-generic/errno.h \
+  include/asm-generic/errno-base.h \
+  include/linux/elf.h \
+  include/linux/auxvec.h \
+  include/asm/auxvec.h \
+  include/linux/elf-em.h \
+  include/asm/elf.h \
+  include/asm/ptrace.h \
+  include/asm/ptrace-abi.h \
+  include/asm/user.h \
+  include/linux/kobject.h \
+    $(wildcard include/config/hotplug.h) \
+  include/linux/sysfs.h \
+    $(wildcard include/config/sysfs.h) \
+  include/linux/rwsem.h \
+    $(wildcard include/config/rwsem/generic/spinlock.h) \
+  include/linux/rwsem-spinlock.h \
+  include/linux/kref.h \
+  include/linux/wait.h \
+  include/linux/moduleparam.h \
+  include/linux/init.h \
+    $(wildcard include/config/memory/hotplug.h) \
+    $(wildcard include/config/acpi/hotplug/memory.h) \
+    $(wildcard include/config/acpi/hotplug/memory/module.h) \
+  include/asm/local.h \
+  include/linux/percpu.h \
+  include/linux/slab.h \
+    $(wildcard include/config/slab/debug.h) \
+    $(wildcard include/config/slab.h) \
+    $(wildcard include/config/debug/slab.h) \
+  include/linux/gfp.h \
+    $(wildcard include/config/zone/dma32.h) \
+    $(wildcard include/config/highmem.h) \
+  include/linux/mmzone.h \
+    $(wildcard include/config/force/max/zoneorder.h) \
+    $(wildcard include/config/arch/populates/node/map.h) \
+    $(wildcard include/config/flat/node/mem/map.h) \
+    $(wildcard include/config/have/memory/present.h) \
+    $(wildcard include/config/need/node/memmap/size.h) \
+    $(wildcard include/config/need/multiple/nodes.h) \
+    $(wildcard include/config/have/arch/early/pfn/to/nid.h) \
+    $(wildcard include/config/sparsemem/extreme.h) \
+    $(wildcard include/config/nodes/span/other/nodes.h) \
+  include/linux/numa.h \
+    $(wildcard include/config/nodes/shift.h) \
+  include/linux/nodemask.h \
+  include/linux/memory_hotplug.h \
+    $(wildcard include/config/have/arch/nodedata/extension.h) \
+  include/linux/notifier.h \
+  include/linux/mutex.h \
+    $(wildcard include/config/debug/mutexes.h) \
+  include/linux/srcu.h \
+  include/linux/topology.h \
+    $(wildcard include/config/sched/smt.h) \
+    $(wildcard include/config/sched/mc.h) \
+  include/linux/smp.h \
+  include/asm/smp.h \
+  include/asm/fixmap.h \
+  include/asm/apicdef.h \
+  include/asm/vsyscall.h \
+  include/asm/vsyscall32.h \
+  include/asm/mpspec.h \
+    $(wildcard include/config/acpi.h) \
+  include/asm/io_apic.h \
+  include/asm/apic.h \
+    $(wildcard include/config/x86/good/apic.h) \
+  include/linux/pm.h \
+    $(wildcard include/config/pm.h) \
+  include/asm/topology.h \
+    $(wildcard include/config/acpi/numa.h) \
+  include/asm-generic/topology.h \
+  include/asm/mmzone.h \
+  include/linux/slab_def.h \
+  include/linux/kmalloc_sizes.h \
+    $(wildcard include/config/mmu.h) \
+    $(wildcard include/config/large/allocs.h) \
+  include/asm/module.h \
+  include/linux/vermagic.h \
+  include/linux/utsrelease.h \
+
+/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.o: $(deps_/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.o)
+
+$(deps_/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.mod.o):
---
+++ plugins/abb/linuxdrivers/pciDriver2/src/driver/.tmp_versions/pciDriver.mod
@@ -0,0 +1,2 @@
+/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver.ko
+/u/linev/smb/workspace/dabc/plugins/abb/linuxdrivers/pciDriver2/src/driver/pciDriver_base.o
