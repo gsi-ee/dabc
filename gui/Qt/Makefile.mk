892fc8e2a539fec0fddcb2bfc067ebef8d85b244
Joern Adamczewski <j.adamczewski@gsi.de>
2008-11-05 09:44:04 +0000

    git-svn-id: https://subversion.gsi.de/dabc/trunk@56 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- gui/Qt/Makefile.mk
+++ gui/Qt/Makefile.mk
@@ -1,7 +1,21 @@
+ifdef QTDIR
+
+ifneq ($(wildcard $(QTDIR)),)
+DABCQTGUI  = true
+endif
 
 exes::
-	@echo "Compile Qt gui here"
+ifdef DABCQTGUI
+	@echo "Compiling Qt gui..."
+	@cd $(DABCSYS)/gui/Qt/mbs-configurator; $(MAKE)
+else
+	@echo QTDIR is set to $(QTDIR), but could not find Qt installation there. Skip Qt gui.
+endif
 
 clean::	
-	@echo "clean Qt gui here"
-	
+ifdef DABCQTGUI
+	@echo "Cleaning Qt gui..."
+	@cd $(DABCSYS)/gui/Qt/mbs-configurator; $(MAKE) clean
+endif
+
+endif	
--- gui/java/Makefile.mk
+++ gui/java/Makefile.mk
@@ -1,7 +1,20 @@
+ifdef DIMDIR
+
+ifneq ($(wildcard $(DIMDIR)),)
+USEDIM  = true
+endif
+
 
 exes::
+ifdef USEDIM
 	@echo "Compiling java gui..."
 	@cd $(DABCSYS)/gui/java/generic; $(MAKE) all;  $(MAKE) jar; 
+else
+	@echo "DIM not set, do not build java gui"
+endif
+
 clean::
 	@echo "Cleaning java gui..."
 	@cd $(DABCSYS)/gui/java/generic; $(MAKE) clean; 
+	
+endif
