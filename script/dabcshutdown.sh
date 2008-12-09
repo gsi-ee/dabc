9a45713f313be1e5eef702f600b1ffa471aa7199
Joern Adamczewski <j.adamczewski@gsi.de>
2008-12-09 15:35:54 +0000

    dabc native version (using run.sh) of the dabcshutdown.sh invoked by java gui
    git-svn-id: https://subversion.gsi.de/dabc/trunk@181 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- script/dabcshutdown.sh
+++ script/dabcshutdown.sh
@@ -18,38 +18,13 @@ export DABC_XDAQ_CONFIG=$3
 export DIM_DNS_NODE=$4
 export DABC_GUI_NODE=$5
 cd $DABC_USER_DIR
-############# NEW: get nodes from setup file
-TARGETSTR="<xc:Context"  
-CONTEXTLIST=`grep "$TARGETSTR" "$DABC_XDAQ_CONFIG" ` 
-#echo found contextlist $CONTEXTLIST
-declare -i SUBCOUNT
-SUBCOUNT=0
-IFSVERYOLD=$IFS
-IFS=$'\012' 
-for FULLNAME in $CONTEXTLIST;
-    do
-        let "SUBCOUNT=0"; 
-        NODENAME=`echo defaultnode`
-        IFSOLD=$IFS
-        IFS=':'
-        for SUBNAME in $FULLNAME
-            do
-                #echo testing $SUBNAME from $FULLNAME
-                    if [[ "$SUBCOUNT" -eq "2" ]]; then                        
-                        NODENAME=`echo $SUBNAME | awk '{ gsub("//","") ; print }'`
-                        #echo "using node  $NODENAME"
-                    fi  
-                let "SUBCOUNT+=1"; 
-            done
-        IFS=$IFSOLD
-#    echo killing all xdaq applications on node $NODENAME ...   
-#    ssh -l $USER $NODENAME  $DABCSYS/script/killxdaq.sh xdaq &
-    echo killing all dabc_run applications on node $NODENAME ...   
-    ssh -l $USER $NODENAME  $DABCSYS/script/killxdaq.sh dabc_run &
-    echo        ready.
 
-done
-IFS=$IFSVERYOLD
+
+echo   dabcshutdown for plain dim control....
+#printenv 
+# $5 $6 $7 $8
+$DABCSYS/bin/run.sh $DABC_XDAQ_CONFIG kill -v 
+
 echo        dabcshutdown is complete. ---
 
 
