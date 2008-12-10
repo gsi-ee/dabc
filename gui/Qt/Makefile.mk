bd1def0badb3eb18d2132a6c6b96f390dee9dee0
Joern Adamczewski <j.adamczewski@gsi.de>
2008-12-10 10:13:31 +0000

    disabled due to designer problems with new Qt. TODO: adjust templates for new xml scheme
    git-svn-id: https://subversion.gsi.de/dabc/trunk@190 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- gui/Qt/Makefile.mk
+++ gui/Qt/Makefile.mk
@@ -1,21 +1,23 @@
-ifdef QTDIR
-
-ifneq ($(wildcard $(QTDIR)/bin/qmake),)
-DABCQTGUI  = true
-endif
-
-exes::
-ifdef DABCQTGUI
-	@echo "Compiling Qt gui..."
-	@cd $(DABCSYS)/gui/Qt/mbs-configurator; $(MAKE)
-else
-	@echo QTDIR is set to $(QTDIR), but could not find required Qt dev installation there. Skip Qt gui.
-endif
-
-clean::	
-ifdef DABCQTGUI
-	@echo "Cleaning Qt gui..."
-	@cd $(DABCSYS)/gui/Qt/mbs-configurator; $(MAKE) clean
-endif
-
-endif	
+exes::	
+	@echo ----------- build of Qt mbs-configurator currently disabled
+#ifdef QTDIR
+#
+#ifneq ($(wildcard $(QTDIR)/bin/qmake),)
+#DABCQTGUI  = true
+#endif
+#
+#exes::
+#ifdef DABCQTGUI
+#	@echo "Compiling Qt gui..."
+#	@cd $(DABCSYS)/gui/Qt/mbs-configurator; $(MAKE)
+#else
+#	@echo QTDIR is set to $(QTDIR), but could not find required Qt dev installation there. Skip Qt gui.
+#endif
+#
+#clean::	
+#ifdef DABCQTGUI
+#	@echo "Cleaning Qt gui..."
+#	@cd $(DABCSYS)/gui/Qt/mbs-configurator; $(MAKE) clean
+#endif
+#
+#endif	
