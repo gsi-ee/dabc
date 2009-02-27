6fbf9fb4f25ad1e709c52accf0d12afd176e4f8c
Sergey Linev <S.Linev@gsi.de>
2009-02-27 16:48:02 +0000

    modify java doc makefiles
    git-svn-id: https://subversion.gsi.de/dabc/trunk@549 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- gui/java/Makefile.mk
+++ gui/java/Makefile.mk
@@ -5,10 +5,14 @@ exes::
 	@echo "Compiling java gui..."
 	cd $(DABCSYS)/gui/java/generic; export DIMDIR=$(DIMDIR); make -j1 all 
 
+docs::
+	@echo "Compiling java gui docs..."
+	cd $(DABCSYS)/gui/java/generic; export DIMDIR=$(DIMDIR); make -j1 doc 
+
 #endif
 
 clean::
 	@echo "Cleaning java gui..."
-	@cd $(DABCSYS)/gui/java/generic; $(MAKE) clean; 
+	@cd $(DABCSYS)/gui/java/generic; $(MAKE) clean; $(MAKE) cleandoc
 	
 endif
