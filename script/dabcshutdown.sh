526c5f7bd496ec402b9fa9c87aa4eac8f0904858
Sergey Linev <S.Linev@gsi.de>
2009-03-05 13:11:47 +0000

    Force output of startup/shutdown scripts into /dev/null
    git-svn-id: https://subversion.gsi.de/dabc/trunk@571 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- script/dabcshutdown.sh
+++ script/dabcshutdown.sh
@@ -5,11 +5,11 @@
 #  3 DIM_DNS_NODE
 #  4 GuiNode
 #  5-9 others
-echo ---- dabcshutdown called with:
-echo $1
-echo $2
-echo $3
-echo $4
+#echo ---- dabcshutdown called with:
+#echo $1
+#echo $2
+#echo $3
+#echo $4
 #echo $5
 
 export DABCSYS=$1
@@ -19,13 +19,9 @@ export DIM_DNS_NODE=$4
 export DABC_GUI_NODE=$5
 cd $DABC_USER_DIR
 
-echo   dabcshutdown for plain dim control....
 #printenv 
 # $5 $6 $7 $8
-$DABCSYS/bin/run.sh $DABC_XDAQ_CONFIG $6 
-
-echo        dabcshutdown is complete. ---
-
+$DABCSYS/bin/run.sh $DABC_XDAQ_CONFIG $6  >/dev/null 2>/dev/null
 
 
 
--- script/dabcstartup.sh
+++ script/dabcstartup.sh
@@ -6,12 +6,12 @@
 #  4 DIM_DNS_NODE
 #  5 GuiNode
 #  5-9 others
-echo ---- dabcstartup called with:
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
+#echo ---- dabcstartup called with:
+#echo $1
+#echo $2
+#echo $3
+#echo $4
+#echo $5
 
 export DABCSYS=$1
 export DABC_USER_DIR=$2
@@ -21,8 +21,8 @@ export DABC_XDAQ_CONFIG=$3
 #DABC_MBS_Setup.xml
 #export PATH=$DABCSYS/bin:$PATH
 cd $DABC_USER_DIR
-echo   dabcstartup for plain dim control....
+#echo   dabcstartup for plain dim control....
 #printenv 
 # $5 $6 $7 $8
-$DABCSYS/bin/run.sh $DABC_XDAQ_CONFIG start -dim
-echo        dabcstartup is complete. --- 
+$DABCSYS/bin/run.sh $DABC_XDAQ_CONFIG start -dim >/dev/null 2>/dev/null
+#echo        dabcstartup is complete. --- 
