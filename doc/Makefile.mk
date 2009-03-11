5612420c2837d463194d6bb6ce9c35f30bf5395b
Sergey Linev <S.Linev@gsi.de>
2009-03-11 17:01:41 +0000

    Add generation of user manual by 'make docs' rule
    git-svn-id: https://subversion.gsi.de/dabc/trunk@600 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- doc/Makefile.mk
+++ doc/Makefile.mk
@@ -2,9 +2,8 @@
 docs::
 	@echo "Making docs ..."
 	cd $(DABCSYS)/doc; export TEXINPUTS=$(DABCSYS)/doc//:; export BIBINPUTS=$(DABCSYS)/doc; make;
-
+	cd $(DABCSYS)/doc; export TEXINPUTS=$(DABCSYS)/doc//:; export BIBINPUTS=$(DABCSYS)/doc; makedoc user
 
 clean::
 	@echo "Cleaning docs ..."
-	@cd $(DABCSYS)/doc; make clean; 
-	
+	@cd $(DABCSYS)/doc; make clean;
