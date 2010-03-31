d8dba1ee73d16600d7ad0a619294278771203f9d
Hans Essel <H.Essel@gsi.de>
2010-03-31 17:19:22 +0000

    Prints disconnect/reconnect
    git-svn-id: https://subversion.gsi.de/dabc/trunk@739 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- applications/mbsmoni/mMbsDimStatus.cxx
+++ applications/mbsmoni/mMbsDimStatus.cxx
@@ -8,24 +8,34 @@
 
 #include "MbsNodeDimStatus.h"
 extern "C" {int f_mbs_status(char *, s_daqst *);}
+extern "C" {char *f_ut_time(char *);}
 
 /**************************************************************************/
 int main(int argc,char **argv)
 {
 INTS4 i,l_status, l_sec=1, l_1st=1, l_delay=0, listc=0;
-char c_dim[128], c_host[128],c_nodelist[512];
+char c_dim[128], c_host[128],c_nodelist[512],c_time[64];
 char c_times[128],c_line[64],c_setup[256],*pc_nodes=0,*pc;
 s_daqst *ps_daqst[20];
+int active[20];
 MbsNodeDimStatus *node[20], *server;
 char list[20][32];
 strcpy(c_line,HISTOGRAMDESC);
 strcpy(c_line,STATEDESC);
 strcpy(c_line,RATEDESC);
 strcpy(c_line,INFODESC);
+memset(active,0,sizeof(active));
 
 if(argc == 1){
 	system("cat $DABCSYS/applications/mbsmoni/readme.txt");
-	cout << "mbsmoni [-t sec] [-s setupfile] [@]node [node2 [node3 ...]]" << endl;
+	exit(0);
+}
+if(strstr(argv[l_1st],"-?") !=0) {
+	system("cat $DABCSYS/applications/mbsmoni/readme.txt");
+	exit(0);
+}
+if(strstr(argv[l_1st],"-h") !=0) {
+	system("cat $DABCSYS/applications/mbsmoni/readme.txt");
 	exit(0);
 }
 if(getenv("DIM_DNS_NODE")==0){
@@ -81,6 +91,7 @@ for(i=0;i<listc;i++){
 	ps_daqst[i] = (s_daqst *) malloc (sizeof( s_daqst));
 	memset(ps_daqst[i],0,sizeof(s_daqst));
 	node[i] = new MbsNodeDimStatus(list[i],ps_daqst[i],"", c_setup);
+	active[i]=1;
 }
 // start serving
 sprintf(c_dim,"%s_%s_%s",list[0],c_host,getenv("LOGNAME"));
@@ -92,19 +103,25 @@ cout<<"Node list file: "<<pc_nodes<<endl;
 cout<<"Scan MBS nodes ["<<l_sec<<" sec]: "<<c_nodelist<<endl;
 // loop over nodes, read status, update DIM services
 while(1){
-	strcpy(c_times,"");
+	f_ut_time(c_time);
+	pc=c_time+10; // time only
+	strcpy(c_times,pc);
+	strcat(c_times,": ");
 	for(i=0;i<listc;i++){
 		l_status = f_mbs_status (list[i],ps_daqst[i]);
 		if(l_status != 1) { // STC__SUCCESS
-			if(l_status == -2)
-				cout<<setw(3)<<l_delay<<" Error connecting node "<<list[i]<<endl;
-			else if(l_status == -1)
-				cout<<setw(3)<<l_delay<<" Error version mismatch "<<list[i]<<endl;
-			else
-				cout<<setw(3)<<l_delay<<" Error connecting node "<<list[i]<<" "<<l_status<<endl;
+			if(l_status == -2){
+				if(active[i]==1)cout<<" Error connecting node  "<<list[i]<<endl;
+			} else if(l_status == -1){
+				if(active[i]==1)cout<<" Error version mismatch "<<list[i]<<endl;
+			} else
+				if(active[i]==1)cout<<" Error connecting node  "<<list[i]<<" "<<l_status<<endl;
 			node[i]->Reset(); // clear daqst
-			l_delay += l_sec; // for printout
-		} else l_delay=0;
+			active[i]=0;
+		} else {
+			if(active[i]==0)    cout<<" Reconnected node       "<<list[i]<<endl;
+			active[i]=1;
+		}
 		node[i]->Update("");
 		pc = ps_daqst[i]->c_date+13; // only min:sec
 		strcat(c_times,pc);strcat(c_times," ");
