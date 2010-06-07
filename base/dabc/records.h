03d4b75d06e84a29c9707359bf66fbe3c015cc06
Joern Adamczewski <j.adamczewski@gsi.de>
2010-06-07 15:04:46 +0000

    Adjusted records.h for use with external plain C programs (EPICS)
    git-svn-id: https://subversion.gsi.de/dabc/trunk@812 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- base/dabc/records.h
+++ base/dabc/records.h
@@ -27,47 +27,47 @@
 #define _DABC_COLOR_MAGENTA_ 	"Magenta"
 
 
-#ifdef __cplusplus // compatibility mode for pure C includes
+#ifdef __cplusplus /* compatibility mode for pure C includes */
 
 namespace dabc {
 
 #endif
 
    typedef struct{
-      int channels;  // channels of data
+      int channels;  /* channels of data */
       float xlow;
       float xhigh;
       char xlett[32];
       char cont[32];
-      char color[16]; // (color name: White, Red, Green, Blue, Cyan, Yellow, Magenta)
-      int data; // first data word. 
-     // DIM record must be allocated by malloc, including this header and the data field.
+      char color[16]; /* (color name: White, Red, Green, Blue, Cyan, Yellow, Magenta) */
+      int data; /* first data word. */
+     /* DIM record must be allocated by malloc, including this header and the data field. */
    } HistogramRec;
 
    typedef struct{
-      int severity; // (0=success, 1=warning, 2=error, 3=fatal)
-      char color[16]; // (color name: Red, Green, Blue, Cyan, Yellow, Magenta)
-      char status[16]; // status name
+      int severity; /* (0=success, 1=warning, 2=error, 3=fatal)*/
+      char color[16]; /* (color name: Red, Green, Blue, Cyan, Yellow, Magenta)*/
+      char status[16]; /* status name */
    } StatusRec;
 
 
     typedef struct{
-    int verbose; //(0=Plain text, 1=Node:text)
-    char color[16]; //(Red, Green, Blue, Cyan, Magenta, Yellow)
-    char info[128]; // info message
+    int verbose; /* (0=Plain text, 1=Node:text) */
+    char color[16]; /* (Red, Green, Blue, Cyan, Magenta, Yellow) */
+    char info[128]; /* info message */
    } InfoRec;
    
 
 
    typedef struct{
       float value; 
-      int displaymode; // one of the DISPLAY_x
-      float lower; // limit. If limits are equal, use autoscale
-      float upper; // limit
-      float alarmlower; // alarm
-      float alarmupper; // alarm
-      char color[16]; // (color name: Red, Green, Blue, Cyan, Yellow, Magenta)
-      char alarmcolor[16]; // (color name: Red, Green, Blue, Cyan, Yellow, Magenta)
+      int displaymode; /* one of the DISPLAY_x */
+      float lower; /* limit. If limits are equal, use autoscale */
+      float upper; /* limit */
+      float alarmlower; /* alarm */
+      float alarmupper; /* alarm */
+      char color[16]; /* (color name: Red, Green, Blue, Cyan, Yellow, Magenta) */
+      char alarmcolor[16]; /* (color name: Red, Green, Blue, Cyan, Yellow, Magenta) */
       char units[16];
    } RateRec;
 
@@ -91,8 +91,8 @@ namespace dabc {
    extern const char* col_Yellow;
    extern const char* col_Magenta;
 
-} // namespace
-#endif //c++
+} /* namespace */
+#endif /* c++ */
 
 
 #endif
