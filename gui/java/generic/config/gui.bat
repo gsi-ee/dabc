bb1b569a34b75a956cf492093017df9b849a4ccc
Sergey Linev <S.Linev@gsi.de>
2012-01-30 12:48:36 +0000

    Merge 1.1 and 1.9 in the trunk, from this moment only dabc v2 beta is official version
    git-svn-id: https://subversion.gsi.de/dabc/trunk@1228 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- gui/java/generic/config/gui.bat
+++
@@ -1,6 +0,0 @@
-set HOST=%COMPUTERNAME% 
-set USER=%USERNAME%
-set DIM_DNS_NAME=<node>
-set CLASSPATH=%CLASSPATH%;<dim path>/classes;<gui jar file>
-set PATH=%PATH%;<dim path>/bin
-java  -Xmx200m xgui.xGui
