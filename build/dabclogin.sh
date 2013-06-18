f880029652a10a777560997b98ea53fcf252e3cc
Sergey Linev <S.Linev@gsi.de>
2013-06-18 15:22:35 +0000

    Solve several build problems
    git-svn-id: https://subversion.gsi.de/dabc/trunk@1638 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- build/dabclogin.sh
+++ build/dabclogin.sh
@@ -10,27 +10,27 @@ export LD_LIBRARY_PATH=.:$DABCSYS/lib:$LD_LIBRARY_PATH
 
 echo Configure DABC version at $DABCSYS
 
-############### here is JAVA configuration #########################################
+##### here is JAVA configuration, enable it only if java gui will be used #######
 
-export CLASSPATH=.:$DABCSYS/lib/xgui.jar:$DABCSYS/lib/dim.jar
-
-if [[ "x$JAVA_HOME" == "x" ]] ; then 
-   if [[ -d /usr/lib/jvm/java-1.6.0-sun ]] ; then
-      export JAVA_HOME=/usr/lib/jvm/java-1.6.0-sun
-      echo "Set Java location (JAVA_HOME) to $JAVA_HOME"
-   elif [[ -d /usr/lib64/jvm/java-1.6.0-sun ]] ; then
-      export JAVA_HOME=/usr/lib64/jvm/java-1.6.0-sun
-      echo "Set Java location (JAVA_HOME) to $JAVA_HOME"
-   else
-      echo "!!!!!!!!! Java location (JAVA_HOME) not specified - try default !!!!!!!!!"
-      export JAVA_HOME=/usr/lib/jvm/default-java
-   fi
-fi
-
-alias dabc="$JAVA_HOME/bin/java -Xmx400m xgui.xGui"
-
-alias dabcmoni="$JAVA_HOME/bin/java -Xmx400m xgui.xGui -moni"
-
+#export CLASSPATH=.:$DABCSYS/lib/xgui.jar:$DABCSYS/lib/dim.jar
+#
+#if [[ "x$JAVA_HOME" == "x" ]] ; then 
+#   if [[ -d /usr/lib/jvm/java-1.6.0-sun ]] ; then
+#      export JAVA_HOME=/usr/lib/jvm/java-1.6.0-sun
+#      echo "Set Java location (JAVA_HOME) to $JAVA_HOME"
+#   elif [[ -d /usr/lib64/jvm/java-1.6.0-sun ]] ; then
+#      export JAVA_HOME=/usr/lib64/jvm/java-1.6.0-sun
+#      echo "Set Java location (JAVA_HOME) to $JAVA_HOME"
+#   else
+#      echo "!!!!!!!!! Java location (JAVA_HOME) not specified - try default !!!!!!!!!"
+#      export JAVA_HOME=/usr/lib/jvm/default-java
+#   fi
+#fi
+#
+#alias dabc="$JAVA_HOME/bin/java -Xmx400m xgui.xGui"
+#
+#alias dabcmoni="$JAVA_HOME/bin/java -Xmx400m xgui.xGui -moni"
+#
 #echo ">>> Set DIM_DNS_NODE to the node to run the DIM name server"
 #echo ">>> Start DIM name server on that node by dimDns"
 #echo ">>> Start DIM browser on that node by dimDid (recommended)"
