d8a150211c3ee35ad64033756281bb5a399be075
Sergey Linev <S.Linev@gsi.de>
2013-06-20 14:58:44 +0000

    3. Extract DABC-specific JavaScript code to dabc.js. 4. Implement binary requests, central binary callback and    first primitive version of drawing histogram from DABC browser!
    git-svn-id: https://subversion.gsi.de/dabc/trunk@1645 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

---
+++ plugins/http/scripts/dabc.js
@@ -0,0 +1,396 @@
+  DABC = {};
+
+  DABC.version = "2.1.1";
+  
+  DABC.mgr = 0;
+
+  DABC.DrawElement = function() {
+     this.itemname = "";   // full item name in hierarhcy
+     this.frameid = 0;    // id of top frame, where item is drawn
+     return this;
+  }
+
+  // indicates if element operates with simple text fields, 
+  // which can be processed by SetValue() method
+  DABC.DrawElement.prototype.simple = function() { return false; }
+  
+  // method regularly called by the manager
+  // it is responsibility of item itself create request and perform update
+  DABC.DrawElement.prototype.CheckComplexRequest = function() { return; }
+  
+  DABC.DrawElement.prototype.CreateFrames = function(topid,cnt) {
+     this.frameid = "dabc_dummy_" + cnt;
+  
+     var entryInfo = 
+       "<div id='" +this.frameid + "'>" + 
+        "<h2> CreateFrames for item " + this.itemname + " not implemented </h2>"+
+       "</div>"; 
+     $(topid).append(entryInfo);
+  }
+  
+  DABC.DrawElement.prototype.SetValue = function(val) {
+     if (!val || !this.frameid) return;
+     document.getElementById(this.frameid).innerHTML = "Value = " + val;
+  }
+  
+
+  // ======== end of DrawElement ======================
+
+  // ======== start of GaugeDrawElement ======================
+  
+  DABC.GaugeDrawElement = function() {
+     DABC.DrawElement.call(this);
+     this.gauge = 0;
+  }
+  
+  // TODO: check how it works in different older browsers
+  DABC.GaugeDrawElement.prototype = Object.create( DABC.DrawElement.prototype );
+
+  DABC.GaugeDrawElement.prototype.simple = function() { return true; }
+
+  DABC.GaugeDrawElement.prototype.CreateFrames = function(topid, id) {
+
+     this.frameid = "dabc_gauge_" + id;
+
+     var entryInfo = 
+       "<div id='"+this.frameid+ "' class='200x160px'> </div> \n";
+     $(topid).append(entryInfo);
+
+     this.gauge = new JustGage({
+         id: this.frameid, 
+         value: 0,
+         min: 0,
+         max: 100,
+         title: this.itemname
+     }); 
+  }
+
+  DABC.GaugeDrawElement.prototype.SetValue = function(val) {
+     if (val) this.gauge.refresh(val);
+  }
+
+  
+  // ======== end of GaugeDrawElement ======================
+
+
+  // ======== start of RootDrawElement ======================
+  
+  DABC.RootDrawElement = function(_clname) {
+     DABC.DrawElement.call(this);
+
+     this.clname = _clname; // ROOT class name
+     this.obj = 0;          // object itself
+     this.sinfo = 0;        // used to refer to the streamer info record
+     this.ready = false;
+     this.req = 0;          // this is current request
+     this.titleid = "";     
+     this.drawid = 0;       // numeric id of pad, where ROOT object is drawn 
+  }
+  
+  // TODO: check how it works in different older browsers
+  DABC.RootDrawElement.prototype = Object.create( DABC.DrawElement.prototype );
+  
+  DABC.RootDrawElement.prototype.CreateFrames = function(topid, id) {
+	 this.drawid = id;
+	 this.frameid = "histogram" + this.drawid;
+	 this.titleid = "uid_accordion_" + this.drawid; 
+     var entryInfo = "<h5 id='"+this.titleid+"'><a>" + this.itemname + "</a>&nbsp; </h5>\n";
+     entryInfo += "<div id='" + this.frameid + "'>\n";
+     $(topid).append(entryInfo);
+  }
+  
+  DABC.RootDrawElement.prototype.BinaryCallback = function(arg) {
+	 // in any case, request pointer will be reseted
+	  this.req = 0;
+	  
+	  if (!arg) {
+	 	 $("#report").append("<br> RootDrawElement get callback " + this.itemname + " error");
+	 	 return; 
+	  }
+	  
+ 	  // $("#report").append("<br> RootDrawElement get callback " + this.itemname + " sz " + arg.length);
+	 
+ 	  if (!this.sinfo) {
+		 // we are doing sreamer infos
+		 if (gFile) $("#report").append("<br> gFile already exists???"); 
+		     else gFile = new JSROOTIO.RootFile;
+		
+         gFile.fStreamerInfo.ExtractStreamerInfo(arg);
+         
+         // we are done with the reconstructing of streamer infos
+         this.ready = true;
+
+         // with streamer info one could try to update complex fields
+         DABC.mgr.UpdateComplexFields();
+
+         return;
+	  } 
+      // not try to update again
+	  this.ready = true;
+	  
+	  this.obj = {};
+      this.obj['_typename'] = 'JSROOTIO.' + this.clname;
+      
+      // $("#report").append("<br>make streaming " + arg.length + " for class "+ this.clname);
+      
+      if (!JSROOTIO.GetStreamer(this.clname))
+          $("#report").append("<br>!!!!! streamer not found !!!!!!!");
+      
+      JSROOTIO.GetStreamer(this.clname).Stream(this.obj, arg, 0);
+
+      JSROOTCore.addMethods(this.obj);
+
+      // $("#report").append("<br>Object name " + this.obj['fName']+ " created");
+
+      JSROOTPainter.drawObject(this.obj, this.drawid);
+      addCollapsible("#"+this.titleid);
+  }
+  
+  DABC.RootDrawElement.prototype.CheckComplexRequest = function() {
+	 // in any case streamer info is required before normal request can be submitted
+
+	 if (this.sinfo) {
+//         $("#report").append("<br> checking sinfo " + (this.sinfo.ready ? "true" : "false"));
+	 } else {
+//		 $("#report").append("<br> sinfo itself " + (this.ready ? "true" : "false"));
+	 }
+	  
+	 if (this.sinfo && !this.sinfo.ready) return;
+	 
+	 // when streamer info ready, than ignore request 
+	 if (!this.sinfo && this.ready) return;
+	 
+	 // if request is created and running, do nothing
+	 if (this.req) return;
+	 
+	 if (this.ready) return;
+	 
+	 var url;
+	 if (!this.sinfo)
+	    url = "streamerinfo.data";
+	 else
+		url = "binary.data"; 
+	 
+	 this.req = DABC.mgr.NewBinRequest(url, true, this.itemname);
+	 
+     // $("#report").append("<br> Send request " + url);
+	 
+	 this.req.send();
+  }
+  
+  
+  
+  
+  // ======== end of RootDrawElement ======================
+
+  
+  
+  // ============= start of DABC.Manager =============== 
+   
+  DABC.Manager = function(abc) {
+     this.cnt = 0;            // counter to create new element 
+     this.arr = new Array();  // array of DrawElement
+     return this;
+  }
+  
+  DABC.Manager.prototype.FindItem = function(_item) {
+     for (var i in this.arr) {
+        if (this.arr[i].itemname == _item) return this.arr[i];
+     }
+  }
+
+  DABC.Manager.prototype.empty = function() {
+     return this.arr.length == 0;
+  }
+  
+  // this is used for text request 
+  DABC.Manager.prototype.NewRequest = function() {
+    var req;
+    // For Safari, Firefox, and other non-MS browsers
+    if (window.XMLHttpRequest) {
+      try {
+        req = new XMLHttpRequest();
+      } catch (e) {
+        req = false;
+      }
+    } else if (window.ActiveXObject) {
+      // For Internet Explorer on Windows
+      try {
+        req = new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {
+        try {
+          req = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+          req = false;
+        }
+      }
+    }
+    
+    return req;
+  }
+  
+  DABC.Manager.prototype.BinaryCallback = function(itemname, arg) {
+     var item = this.FindItem(itemname);
+	 if (item) item.BinaryCallback(arg);
+  }
+  
+  
+  DABC.Manager.prototype.NewBinRequest = function(url, async, itemname) {
+	  var xhr = new XMLHttpRequest();
+	  
+      if (typeof ActiveXObject == "function") {
+          xhr.onreadystatechange = function() {
+              if (this.readyState == 4 && (this.status == 200 || this.status == 206)) {
+                 var filecontent = new String("");
+                 var array = new VBArray(this.responseBody).toArray();
+                 for (var i = 0; i < array.length; i++) {
+                    filecontent = filecontent + String.fromCharCode(array[i]);
+                 }
+                 DABC.mgr.BinaryCallback(itemname, filecontent);
+                 delete filecontent;
+                 filecontent = null;
+              }
+    	  xhr.open('POST', url, async);  
+      }
+    } else {
+          xhr.onreadystatechange = function() {
+              if (this.readyState == 4 && (this.status == 0 || this.status == 200 ||
+                  this.status == 206)) {
+               var HasArrayBuffer = ('ArrayBuffer' in window && 'Uint8Array' in window);
+               var Buf;
+               if (HasArrayBuffer && 'mozResponse' in this) {
+                   Buf = this.mozResponse;
+               } else if (HasArrayBuffer && this.mozResponseArrayBuffer) {
+                   Buf = this.mozResponseArrayBuffer;
+               } else if ('responseType' in this) {
+                  Buf = this.response;
+               } else {
+                  Buf = this.responseText;
+                  HasArrayBuffer = false;
+               }
+               if (HasArrayBuffer) {
+                  var filecontent = new String("");
+                  var bLen = Buf.byteLength;
+                  var u8Arr = new Uint8Array(Buf, 0, bLen);
+                  for (var i = 0; i < u8Arr.length; i++) {
+                      filecontent = filecontent + String.fromCharCode(u8Arr[i]);
+                  }
+                  delete u8Arr;
+               } else {
+                  var filecontent = Buf;
+               }
+
+               DABC.mgr.BinaryCallback(itemname, filecontent);
+                   
+               delete filecontent;
+               filecontent = null;
+            }
+          }
+
+          xhr.open('POST', url, async);
+          var HasArrayBuffer = ('ArrayBuffer' in window && 'Uint8Array' in window);
+          if (HasArrayBuffer && 'mozResponseType' in xhr) {
+             xhr.mozResponseType = 'arraybuffer';
+          } else if (HasArrayBuffer && 'responseType' in xhr) {
+             xhr.responseType = 'arraybuffer';
+          } else {
+             //XHR binary charset opt by Marcus Granado 2006 [http://mgran.blogspot.com]
+             xhr.overrideMimeType("text/plain; charset=x-user-defined");
+          }
+      }
+      return xhr;
+  }
+  
+  // This method used to update all simple fields together
+  DABC.Manager.prototype.UpdateSimpleFields = function() {
+     if (this.empty()) return;
+
+     var url = "chartreq.htm?";
+     var cnt = 0;
+  
+     for (var i in this.arr) {
+        if (!this.arr[i].simple()) continue;
+        if (cnt++>0) url+="&";
+        url += this.arr[i].itemname;
+     }
+     
+     if (cnt==0) return;
+ 
+     var req = this.NewRequest();
+     if (!req) return;
+
+     // $("#report").append("<br> Send request " + url);
+
+     req.open("POST", url, false);
+     req.send();
+      
+     var repl = JSON && JSON.parse(req.responseText) || $.parseJSON(req.responseText);
+
+     // $("#report").append("<br> Get reply " + req.responseText);
+
+     req = 0;
+     if (!repl) return;
+
+     for (var indx in repl) {
+        var elem = this.FindItem(repl[indx].name);
+        // $("#report").append("<br> Check reply for " + repl[indx].name);
+
+        if (elem) elem.SetValue(repl[indx].value);
+     }
+  }
+  
+  DABC.Manager.prototype.UpdateComplexFields = function() {
+	  if (this.empty()) return;
+
+      for (var i in this.arr) 
+	    if (!this.arr[i].simple())
+	       this.arr[i].CheckComplexRequest();
+  }
+  
+  DABC.Manager.prototype.UpdateAll = function() {
+     this.UpdateSimpleFields();
+     this.UpdateComplexFields();
+  }
+  
+  
+  DABC.Manager.prototype.click = function(item) {
+     var itemname = item.getAttribute("fullname");
+     
+     if (this.FindItem(itemname)) return;
+     
+     var kind = item.getAttribute("kind");
+
+     var elem = 0;
+     
+     if (kind == "rate") {
+       elem = new DABC.GaugeDrawElement();
+     } else
+     if (kind.indexOf("ROOT.") == 0) {
+        // this element is only required 
+        var sinfo = this.FindItem("streamerinfo.bin"); 
+        if (!sinfo) {
+           AssertPrerequisites(function() { $("#report").append("Loading JSRootIO done<br>"); });
+           
+           sinfo = new DABC.RootDrawElement("TStreamerInfo");
+           sinfo.itemname = "streamerinfo.bin";
+           this.arr.push(sinfo);
+        }
+     
+        elem = new DABC.RootDrawElement(kind.substr(5));
+        elem.sinfo = sinfo;
+     }
+     
+     if (elem==0) return;
+     
+     elem.itemname = itemname;
+     
+     elem.CreateFrames("#report", this.cnt++);
+     
+     elem.SetValue(item.getAttribute("value"));
+     
+     this.arr.push(elem);
+  }
+  
+  
+  
+  // ============= end of DABC.Manager =============== 
