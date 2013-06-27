f7e78941fc94fce99884b274d745ef2ff4cf3bf2
Sergey Linev <S.Linev@gsi.de>
2013-06-27 10:38:29 +0000

    When generating ROOT dictionaries, provide only relevant includes to CINT
    git-svn-id: https://subversion.gsi.de/dabc/trunk@1670 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/root/dabc_root/functions.h
+++ plugins/root/dabc_root/functions.h
@@ -11,27 +11,25 @@
  * This software can be used under the GPL license agreements as stated
  * in LICENSE.txt file which is part of the distribution.
  ********************************************************************/
-#ifndef DABC_ROOT_Factory
-#define DABC_ROOT_Factory
-
-#ifndef DABC_Factory
-#include "dabc/Factory.h"
-#endif
+#ifndef DABC_ROOT_functions
+#define DABC_ROOT_functions
 
 namespace dabc_root {
 
-   class Factory : public dabc::Factory {
-      public:
-         Factory(const std::string& name) : dabc::Factory(name) {}
-
-         virtual void Initialize();
-
-         virtual dabc::Reference CreateObject(const std::string& classname, const std::string& objname, dabc::Command cmd);
-
-         virtual dabc::DataInput* CreateDataInput(const std::string&);
-
-         virtual dabc::DataOutput* CreateDataOutput(const std::string&);
+   /** This is dummy class, required that rootmap file is not empty and
+    * ROOT knows which extra library should be load */
+   class Test {
+       Test() {}
+       ~Test() {}
    };
+
+
+   /** Method need to start HTTP server in ROOT session */
+   bool StartHttpServer(int port = 8080);
+
+   /** Method need to connect to master application,
+    * which is normally HTTP server for many client processes */
+   bool ConnectMaster(const char* master_url);
 
 }
 
--- plugins/root/src/Factory.cxx
+++ plugins/root/src/Factory.cxx
@@ -20,6 +20,7 @@
 #include "dabc/Manager.h"
 #include "dabc/Configuration.h"
 
+#include "dabc_root/functions.h"
 #include "dabc_root/RootSniffer.h"
 
 dabc::FactoryPlugin dabc_root_factory(new dabc_root::Factory("dabc_root"));
@@ -65,10 +66,9 @@ dabc::DataOutput* dabc_root::Factory::CreateDataOutput(const std::string& typ)
 }
 
 
-
-void dabc_root::StartHttpServer(int port)
+bool dabc_root::StartHttpServer(int port)
 {
-   if (!dabc::mgr.null()) return;
+   if (!dabc::mgr.null()) return false;
 
    static dabc::Configuration dabc_root_cfg;
 
@@ -87,7 +87,7 @@ void dabc_root::StartHttpServer(int port)
    cmd2.SetBool("enabled", true);
    cmd2.SetBool("batch", false);
 
-   if (!dabc::mgr.Execute(cmd2)) return;
+   if (!dabc::mgr.Execute(cmd2)) return false;
 
    dabc::WorkerRef w2 = cmd2.GetRef("Object");
 
@@ -105,17 +105,86 @@ void dabc_root::StartHttpServer(int port)
    cmd1.SetStr("top", "ROOT");
    cmd1.SetStr("select", "ROOT");
 
-   if (!dabc::mgr.Execute(cmd1)) return;
+   if (!dabc::mgr.Execute(cmd1)) return false;
 
    dabc::WorkerRef w1 = cmd1.GetRef("Object");
    w1.MakeThreadForWorker("MainThread");
 
    DOUT2("Create http server");
+   return true;
 }
 
-void dabc_root::ConnectMaster(const char* master_url)
+bool dabc_root::ConnectMaster(const char* master_url)
 {
+   if (!dabc::mgr.null()) return false;
 
+   if ((master_url==0) || (strlen(master_url)==0)) {
+      EOUT("Master name is not specified!!!");
+      return false;
+   }
+
+   static dabc::Configuration dabc_root_cfg;
+
+   new dabc::Manager("dabc", &dabc_root_cfg);
+
+   dabc::mgr.SyncWorker();
+
+   DOUT0("Create manager");
+
+   // dabc::mgr.Execute("InitFactories");
+
+   dabc::mgr.CreateThread("MainThread");
+
+   dabc::CmdCreateObject cmd2("dabc_root::RootSniffer","/ROOT");
+   cmd2.SetBool("enabled", true);
+   cmd2.SetBool("batch", false);
+
+   if (!dabc::mgr.Execute(cmd2)) return false;
+
+   dabc::WorkerRef w2 = cmd2.GetRef("Object");
+
+   dabc_root::RootSniffer* sniff = dynamic_cast<dabc_root::RootSniffer*> (w2());
+   if (sniff) sniff->InstallSniffTimer();
+
+   w2.MakeThreadForWorker("MainThread");
+
+   DOUT0("Create root sniffer");
+
+   dabc::mgr()->CreateControl(false);
+
+
+   std::string master = master_url;
+   if (master.find("dabc://") != 0) master = std::string("dabc://") + master;
+
+   DOUT0("Create command channel for %s ", master.c_str());
+
+   dabc::Command cmd("Ping");
+   cmd.SetReceiver(master);
+   cmd.SetTimeout(5.);
+
+   int res = dabc::mgr.GetCommandChannel().Execute(cmd);
+
+   if (res != dabc::cmd_true) {
+      DOUT0("FAIL connection to %s", master.c_str());
+      return false;
+   }
+
+   DOUT0("Connection to %s done", master.c_str());
+
+   dabc::Command cmd3("AcceptClient");
+   cmd3.SetReceiver(master);
+   cmd3.SetTimeout(5.);
+
+   int res3 = dabc::mgr.GetCommandChannel().Execute(cmd3);
+
+   if (res3 != dabc::cmd_true) {
+      DOUT0("FAIL to activate server-client relation to %s", master.c_str());
+      return false;
+   }
+
+   DOUT0("Master %s get control over local process!!!", master.c_str());
+
+   return true;
 }
 
 
