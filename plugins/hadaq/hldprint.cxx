cfbf3ea2e0f842f91dcb3c56579edd69ceaaf6ed
Sergey Linev <S.Linev@gsi.de>
2014-02-24 11:49:20 +0000

    Use mbs:// as default protocol in mbsprint and hldprint
    git-svn-id: https://subversion.gsi.de/dabc/trunk@2023 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/hadaq/hldprint.cxx
+++ plugins/hadaq/hldprint.cxx
@@ -19,6 +19,7 @@
 
 #include "hadaq/api.h"
 #include "dabc/string.h"
+#include "dabc/Url.h"
 
 int usage(const char* errstr = 0)
 {
@@ -30,16 +31,17 @@ int usage(const char* errstr = 0)
    printf("hldprint source [args]\n\n");
    printf("Following source kinds are supported:\n");
    printf("   hld://path/file.hld         - HLD file reading\n");
-   printf("   //path/file.hld             - HLD file reading (file extension MUST be '.hld')\n");
+   printf("   file.hld                    - HLD file reading (file extension MUST be '.hld')\n");
    printf("   mbs://dabcnode/Stream       - DABC stream server\n");
    printf("   mbs://dabcnode:port/Stream  - DABC stream server with custom port\n");
    printf("   mbs://dabcnode/Transport    - DABC transport server\n");
    printf("   lmd://path/file.lmd         - LMD file reading\n");
    printf("Additional arguments:\n");
-   printf("   -tmout value            - maximal time in second for waiting next event\n");
+   printf("   -tmout value            - maximal time in seconds for waiting next event (default 5)\n");
    printf("   -num number             - number of events to print (default 10)\n");
    printf("   -sub                    - try to scan for subsub events (default false)\n");
    printf("   -raw                    - printout of raw data (default false)\n");
+   printf("   -rate                   - display only events rate\n");
 
    return errstr ? 1 : 0;
 }
@@ -49,26 +51,47 @@ int main(int argc, char* argv[])
 {
    if (argc<2) return usage();
 
-   int number = 10;
-   double tmout = 1.;
+   long number = 10;
+   double tmout = 5.;
    bool printraw = false;
    bool printsub = false;
+   bool showrate = false;
 
    int n = 1;
    while (++n<argc) {
-      if ((strcmp(argv[n],"-num")==0) && (n+1<argc)) { dabc::str_to_int(argv[++n], &number); } else
+      if ((strcmp(argv[n],"-num")==0) && (n+1<argc)) { dabc::str_to_lint(argv[++n], &number); } else
       if ((strcmp(argv[n],"-tmout")==0) && (n+1<argc)) { dabc::str_to_double(argv[++n], &tmout); } else
       if (strcmp(argv[n],"-raw")==0) { printraw = true; } else
       if (strcmp(argv[n],"-sub")==0) { printsub = true; } else
+      if (strcmp(argv[n],"-rate")==0) { showrate = true; } else
       if ((strcmp(argv[n],"-help")==0) || (strcmp(argv[n],"?")==0)) return usage(); else
       return usage("Unknown option");
    }
 
-   std::string src = argv[1];
-   if ((src.find(".hld") != std::string::npos) && (src.find("hld://") != 0))
-      src = std::string("hld://") + src;
+   printf("Try to open %s\n", argv[1]);
 
-   printf("Try to open %s\n", src.c_str());
+   bool ishld = false;
+   std::string src = argv[1];
+   if ((src.find(".hld") != std::string::npos) && (src.find("hld://") != 0)) {
+      src = std::string("hld://") + src;
+      ishld = true;
+   }
+
+   if ((src.find("hld://") == 0) || (src.find(".hld") != std::string::npos)) ishld = true;
+
+   if (!ishld) {
+
+      dabc::Url url(src);
+
+      if (url.IsValid()) {
+         if (url.GetProtocol().empty())
+            src = std::string("mbs://") + src;
+
+         if (url.GetFileName().empty())
+            src += "/Stream";
+      }
+   }
+
 
    hadaq::ReadoutHandle ref = hadaq::ReadoutHandle::Connect(src.c_str());
 
@@ -76,9 +99,28 @@ int main(int argc, char* argv[])
 
    hadaq::RawEvent* evnt(0);
 
-   int cnt = 0;
+   long cnt(0), lastcnt(0);
+   dabc::TimeStamp last = dabc::Now();
+   dabc::TimeStamp first = last;
 
    while ((evnt = ref.NextEvent(tmout)) != 0) {
+
+      cnt++;
+
+      if (showrate) {
+
+         dabc::TimeStamp curr = dabc::Now();
+         double tm = curr - last;
+
+         if (tm>=0.3) {
+            printf("\rTm:%6.1fs  Ev:%8ld  Rate:%8.2f Ev/s", first.SpentTillNow(), cnt, (cnt-lastcnt)/tm);
+            fflush(stdout);
+            last = curr;
+            lastcnt = cnt;
+         }
+
+         continue;
+      }
 
       evnt->Dump();
       hadaq::RawSubevent* sub = 0;
@@ -102,7 +144,7 @@ int main(int argc, char* argv[])
          }
       }
 
-      if (++cnt >= number) break;
+      if (cnt >= number) break;
    }
 
    ref.Disconnect();
--- plugins/mbs/mbsprint.cxx
+++ plugins/mbs/mbsprint.cxx
@@ -4,6 +4,7 @@
 
 #include "mbs/api.h"
 #include "dabc/string.h"
+#include "dabc/Url.h"
 
 int usage(const char* errstr = 0)
 {
@@ -18,7 +19,7 @@ int usage(const char* errstr = 0)
    printf("   mbs://mbsnode/Transport - MBS transport server\n");
    printf("   mbs://mbsnode/Stream    - MBS stream server\n");
    printf("Additional arguments:\n");
-   printf("   -tmout value            - maximal time in second for waiting next event\n");
+   printf("   -tmout value            - maximal time in seconds for waiting next event (default 5)\n");
    printf("   -num number             - number of events to print\n");
    printf("   -hex                    - print raw data in hex form\n");
    printf("   -dec                    - print raw data in decimal form\n");
@@ -33,10 +34,11 @@ int main(int argc, char* argv[])
 {
    if (argc<2) return usage();
 
-   int number = 10;
-   double tmout = 1.;
+   long number = 10;
+   double tmout = 5.;
 
-   bool printdata(false), ashex(true), aslong(true);
+
+   bool printdata(false), ashex(true), aslong(true), showrate(false);
 
    int n = 1;
    while (++n<argc) {
@@ -44,22 +46,53 @@ int main(int argc, char* argv[])
       if (strcmp(argv[n],"-dec")==0) { printdata = true; ashex = false; } else
       if (strcmp(argv[n],"-long")==0) { printdata = true; aslong = true; } else
       if (strcmp(argv[n],"-short")==0) { printdata = true; aslong = false; } else
-      if ((strcmp(argv[n],"-num")==0) && (n+1<argc)) { dabc::str_to_int(argv[++n], &number); } else
+      if (strcmp(argv[n],"-rate")==0) { showrate = true; } else
+      if ((strcmp(argv[n],"-num")==0) && (n+1<argc)) { dabc::str_to_lint(argv[++n], &number); } else
       if ((strcmp(argv[n],"-tmout")==0) && (n+1<argc)) { dabc::str_to_double(argv[++n], &tmout); } else
       if ((strcmp(argv[n],"-help")==0) || (strcmp(argv[n],"?")==0)) return usage(); else
       return usage("Unknown option");
    }
 
-   mbs::ReadoutHandle ref = mbs::ReadoutHandle::Connect(argv[1]);
+   std::string src = argv[1];
+   dabc::Url url(src);
+
+   if (url.IsValid()) {
+      if (url.GetProtocol().empty())
+         src = std::string("mbs://") + src;
+
+      if (url.GetFileName().empty())
+         src += "/Stream";
+   }
+
+   mbs::ReadoutHandle ref = mbs::ReadoutHandle::Connect(src);
 
    if (ref.null()) return 1;
 
-
    mbs::EventHeader* evnt(0);
 
-   int cnt = 0;
+   long cnt(0), lastcnt(0);
+
+   dabc::TimeStamp last = dabc::Now();
+   dabc::TimeStamp first = last;
 
    while ((evnt = ref.NextEvent(tmout)) != 0) {
+
+      cnt++;
+
+      if (showrate) {
+
+         dabc::TimeStamp curr = dabc::Now();
+         double tm = curr - last;
+
+         if (tm>=0.3) {
+            printf("\rTm:%6.1fs  Ev:%8ld  Rate:%8.2f Ev/s", first.SpentTillNow(), cnt, (cnt-lastcnt)/tm);
+            fflush(stdout);
+            last = curr;
+            lastcnt = cnt;
+         }
+
+         continue;
+      }
 
       evnt->PrintHeader();
       mbs::SubeventHeader* sub = 0;
@@ -68,7 +101,7 @@ int main(int argc, char* argv[])
          if (printdata) sub->PrintData(ashex, aslong);
       }
 
-      if (++cnt >= number) break;
+      if (cnt >= number) break;
    }
 
    ref.Disconnect();
