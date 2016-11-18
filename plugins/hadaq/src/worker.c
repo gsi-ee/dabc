cf4ee9fe065cca76cc5a0aa436a5460c2a1c1fd4
Sergey Linev <S.Linev@gsi.de>
2016-11-18 17:13:07 +0000

    fix gcc6 compilation warnings
    git-svn-id: https://subversion.gsi.de/dabc/trunk@3553 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/hadaq/src/worker.c
+++ plugins/hadaq/src/worker.c
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -27,7 +28,7 @@
 
 #include "worker.h"
 
-static sigReceived = 0;
+static int sigReceived = 0;
 
 static void noopHandler(int sig)
 {
