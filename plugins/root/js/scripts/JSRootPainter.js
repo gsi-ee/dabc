c32b386ebc5604619347b9cb902c69608f51a894
Sergey Linev <S.Linev@gsi.de>
2018-06-20 11:10:02 +0000

    Update JSROOT
    git-svn-id: https://subversion.gsi.de/dabc/trunk@3996 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/root/js/scripts/JSRootPainter.js
+++ plugins/root/js/scripts/JSRootPainter.js
@@ -1767,6 +1767,37 @@
          this.receiver[method](this, arg, arg2);
    }
 
+   /** Provide data for receiver. When no queue - do it directly.
+    * @private */
+   WebWindowHandle.prototype.ProvideData = function(_msg, _len) {
+      if (!this.msgqueue)
+         return this.InvokeReceiver("OnWebsocketMsg", _msg, _len);
+      this.msgqueue.push({ ready: true, msg: _msg, len: _len});
+   }
+
+   /** Reserver entry in queue for data, which is not yet decoded.
+    * @private */
+   WebWindowHandle.prototype.ReserveQueueItem = function() {
+      if (!this.msgqueue) this.msgqueue = [];
+      var item = { ready: false, msg: null, len: 0 };
+      this.msgqueue.push(item);
+      return item;
+   }
+
+   /** Reserver entry in queue for data, which is not yet decoded.
+    * @private */
+   WebWindowHandle.prototype.DoneItem = function(item, _msg, _len) {
+      item.ready = true;
+      item.msg = _msg;
+      item.len = _len;
+      while ((this.msgqueue.length > 0) && this.msgqueue[0].ready) {
+         this.InvokeReceiver("OnWebsocketMsg", this.msgqueue[0].msg, this.msgqueue[0].len);
+         this.msgqueue.shift();
+      }
+      if (this.msgqueue.length == 0)
+         delete this.msgqueue;
+   }
+
    /** Close connection. */
    WebWindowHandle.prototype.Close = function(force) {
 
@@ -1888,16 +1919,16 @@
                if (msg instanceof Blob) {
                   // this is case of websocket
                   // console.log('Get Blob object - convert to buffer array');
-                  var reader = new FileReader;
+                  var reader = new FileReader, qitem = pthis.ReserveQueueItem();
                   reader.onload = function(event) {
                      // The file's text will be printed here
-                     pthis.InvokeReceiver('OnWebsocketMsg', event.target.result, 0);
+                     pthis.DoneItem(qitem, event.target.result, 0);
                   }
                   reader.readAsArrayBuffer(msg, e.offset || 0);
                } else {
                   // console.log('got array ' + (typeof msg) + ' len = ' + msg.byteLength);
                   // this is from CEF or LongPoll handler
-                  pthis.InvokeReceiver('OnWebsocketMsg', msg, e.offset || 0);
+                  pthis.ProvideData(msg, e.offset || 0);
                }
 
                return;
@@ -1928,7 +1959,7 @@
             } else if (msg == "$$binary$$") {
                pthis.next_binary = true;
             } else {
-               pthis.InvokeReceiver('OnWebsocketMsg', msg);
+               pthis.ProvideData(msg);
             }
 
             if (pthis.ackn > 7)
@@ -2098,7 +2129,6 @@
     * @private */
 
    TBasePainter.prototype.DrawingReady = function(res_painter) {
-
       this._ready_called_ = true;
       if (this._ready_callback_ !== undefined) {
          var callbacks = this._ready_callback_;
@@ -2114,7 +2144,6 @@
    }
 
    /** @summary Call back will be called when painter ready with the drawing
-    *
     * @private
     */
    TBasePainter.prototype.WhenReady = function(callback) {
@@ -2122,6 +2151,14 @@
       if ('_ready_called_' in this) return JSROOT.CallBack(callback, this);
       if (this._ready_callback_ === undefined) this._ready_callback_ = [];
       this._ready_callback_.push(callback);
+   }
+
+   /** @summary Reset ready state - painter should again call DrawingReady to signal readyness
+   * @private
+   */
+   TBasePainter.prototype.ResetReady = function() {
+      delete this._ready_called_;
+      delete this._ready_callback_;
    }
 
    /** @summary Returns drawn object
@@ -2455,10 +2492,14 @@
       this.draw_g = null; // container for all drawn objects
       this.pad_name = ""; // name of pad where object is drawn
       this.main = null;  // main painter, received from pad
-      this.draw_object = ((obj!==undefined) && (typeof obj == 'object')) ? obj : null;
+      this.AssignObject(obj);
    }
 
    TObjectPainter.prototype = Object.create(TBasePainter.prototype);
+
+   TObjectPainter.prototype.AssignObject = function(obj) {
+      this.draw_object = ((obj!==undefined) && (typeof obj == 'object')) ? obj : null;
+   }
 
    /** @summary Generic method to cleanup painter.
     *
@@ -5490,7 +5531,7 @@
          if (!mathjax && !('as_is' in this.txt)) {
             var arr = txt.split("\n"); txt = "";
             for (var i = 0; i < arr.length; ++i)
-               txt += "<pre>" + arr[i] + "</pre>";
+               txt += "<pre style='margin:0'>" + arr[i] + "</pre>";
          }
 
          var frame = this.select_main(),
