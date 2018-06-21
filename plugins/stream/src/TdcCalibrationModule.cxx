8518fe150e6b6508d864b7f1d22f981fca774dc7
Sergey Linev <S.Linev@gsi.de>
2018-06-21 10:57:14 +0000

    More info at exit
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4010 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/stream/src/TdcCalibrationModule.cxx
+++ plugins/stream/src/TdcCalibrationModule.cxx
@@ -393,7 +393,7 @@ bool stream::TdcCalibrationModule::retransmit()
                   }
 
                   if (tgt && (tgtlen - reslen < iter.subevnt()->GetPaddedSize())) {
-                     EOUT("Not enough space for subevent sz %u in output buffer sz %u seg0 %u filled %u remains %u", iter.subevnt()->GetPaddedSize(), buf.GetTotalSize(), tgtlen, reslen, iter.remained_size());
+                     EOUT("Not enough space for subevent sz %u in output buffer sz %u seg0 %u filled %u remains in src %u", iter.subevnt()->GetPaddedSize(), resbuf.GetTotalSize(), tgtlen, reslen, iter.remained_size());
                      exit(4);
                      return false;
                   }
