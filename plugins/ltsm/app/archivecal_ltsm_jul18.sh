331fa1b0e87e06cbc4297c3866798ff6d696f8cf
Joern Adamczewski <j.adamczewski@gsi.de>
2018-10-22 14:10:33 +0000

    added script to archive calibration files to ltsm plugin
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4128 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

---
+++ plugins/ltsm/app/archivecal_ltsm_jul18.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+############## manually archive TDC calibration files from data disk to ltsm archive #######
+### v 0.1 22-oct-2018 by JAM (j.adamczewski@gsi.de)
+######################################################################################
+RUN=$1
+FOLDER=${RUN}_${HOSTNAME}
+FILE=${FOLDER}.tar.gz
+DATE=$(/bin/date +"%d-%m-%Y %H:%M:%S")
+DESC="manually archived with ltsmc on ${DATE} by ${USER} from ${HOSTNAME}"
+LTSMCBIN="/usr/local/bin/ltsmc"
+BEAMTIME="jul18"
+FILESYS="/lustre/hebe"
+ARCHIVEPATH=${FILESYS}/hades/raw/${BEAMTIME}/default/tsm/cal
+DISKPATH="/home/hadaq/oper/calibr"
+SERVER="lxltsm01"
+NODE="hades"
+PASS="wDhgcvFF7"
+OWNER="hadesdst"
+if [ ! -d "$DISKPATH/$RUN" ]
+then
+	  echo "Folder $RUN not found on $DISKPATH. "
+	  exit;
+fi
+# first pack the cal file
+cd ${DISKPATH}
+mv ${RUN} ${FOLDER}
+/bin/tar cf ${FOLDER}.tar ${FOLDER}/
+/bin/gzip -f  ${FOLDER}.tar
+if [ ! -f "$DISKPATH/$FILE" ]
+then
+	  echo "file $FILE not found on $DISKPATH. "
+	  exit;
+fi
+# archive it with ltsmc pipe mode:
+REV=$(cat "${DISKPATH}/${FILE}" | ${LTSMCBIN} --pipe -n "${NODE}" -f "${FILESYS}" -p "${PASS}" -s "${SERVER}" -o "${OWNER}" -d "'${DESC}'" "${ARCHIVEPATH}/${FILE}")
+#echo cat "${DISKPATH}/${FILE}" 
+#echo ${LTSMCBIN} --pipe -n "${NODE}" -f "${FILESYS}" -p "${PASS}" -s "${SERVER}" -o "${OWNER}" -d "'${DESC}'" "${ARCHIVEPATH}/${FILE}"
+#REV=no
+#
+echo archived file ${DISKPATH}/${FILE} to ${ARCHIVEPATH}/${FILE} with result $REV
