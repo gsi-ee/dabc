64eadbc9d60dd5021a5f496acfebaae45e3c8a55
Joern Adamczewski <j.adamczewski@gsi.de>
2018-10-26 12:16:05 +0000

    Adjust again setup for LTSM storage of hades trb3 tdc calibration files
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4147 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/ltsm/app/archivecal_ltsm_jul18.sh
+++ plugins/ltsm/app/archivecal_ltsm_jul18.sh
@@ -3,9 +3,8 @@
 ### v 0.1 22-oct-2018 by JAM (j.adamczewski@gsi.de)
 ######################################################################################
 RUN=$1
-# Hostname is now appended to folder by dabc at the beginning, so skip moving it:
-#FOLDER=${RUN}_${HOSTNAME}
-FOLDER=${RUN}
+FOLDER=${RUN}_${HOSTNAME}
+#FOLDER=${RUN}
 FILE=${FOLDER}.tar.gz
 DATE=$(/bin/date +"%d-%m-%Y %H:%M:%S")
 DESC="manually archived with ltsmc on ${DATE} by ${USER} from ${HOSTNAME}"
@@ -25,7 +24,7 @@ then
 fi
 # first pack the cal file
 cd ${DISKPATH}
-#mv ${RUN} ${FOLDER}
+mv ${RUN} ${FOLDER}
 /bin/tar cf ${FOLDER}.tar ${FOLDER}/
 /bin/gzip -f  ${FOLDER}.tar
 if [ ! -f "$DISKPATH/$FILE" ]
