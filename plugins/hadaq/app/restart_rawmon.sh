8c4372f406511d75cf1c8af73feb96dde525359a
Joern Adamczewski <j.adamczewski@gsi.de>
2018-11-22 13:38:25 +0000

    adjusted rawmon start script to locate config on data partition
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4163 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/hadaq/app/restart_rawmon.sh
+++ plugins/hadaq/app/restart_rawmon.sh
@@ -3,7 +3,8 @@
 echo Re-starting DAQ Raw data monitor  
 . /home/hadaq/bin/dabclogin  
 . /home/hadaq/bin/streamlogin
-cd /home/hadaq/monitor/hades/dabc
+#cd /home/hadaq/monitor/hades/dabc
+cd /data01/monitor/hades/dabc
 /bin/pidof -x  dabc_exe | /usr/bin/xargs /bin/kill '-s 9' 
 dabc_exe hades.xml > /home/hadaq/oper/DabcRawMon.log 2>&1 &  
 echo DABC Raw DAQ Monitor has been restarted
