4c51b8826e7a19dfd7d8da526603cd13def5e9c9
Sergey Linev <S.Linev@gsi.de>
2019-05-22 15:53:30 +0000

    Use meanwhile standard PTHREAD_MUTEX_RECURSIVE
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4330 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- base/src/threads.cxx
+++ base/src/threads.cxx
@@ -27,7 +27,7 @@ dabc::Mutex::Mutex(bool recursive)
    if (recursive) {
       pthread_mutexattr_t attr;
       pthread_mutexattr_init(&attr);
-      pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
+      pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
       pthread_mutex_init(&fMutex, &attr);
       pthread_mutexattr_destroy(&attr);
    } else
