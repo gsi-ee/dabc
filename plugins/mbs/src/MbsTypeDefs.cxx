b21a43d8728300ed51257a926599a4f6b4031095
Sergey Linev <S.Linev@gsi.de>
2019-05-22 15:33:05 +0000

    Try to replace <byteswap.h> for apple
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4328 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/mbs/src/MbsTypeDefs.cxx
+++ plugins/mbs/src/MbsTypeDefs.cxx
@@ -16,9 +16,17 @@
 #include "mbs/MbsTypeDefs.h"
 
 #include <string.h>
-#include <byteswap.h>
 #include <stdio.h>
 
+#if defined(__APPLE__)
+// Mac OS X / Darwin features
+#include <libkern/OSByteOrder.h>
+#define bswap_16(x) OSSwapInt16(x)
+#define bswap_32(x) OSSwapInt32(x)
+#define bswap_64(x) OSSwapInt64(x)
+#else
+#include <byteswap.h>
+#endif
 
 const char* mbs::protocolLmd          = "lmd";
 const char* mbs::protocolMbs          = "mbs";
