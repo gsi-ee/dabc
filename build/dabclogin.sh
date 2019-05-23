43b0f7facf94df71ce39e595e5ec7cbe31a2e9c2
Sergey Linev <S.Linev@gsi.de>
2019-05-23 14:34:59 +0000

    Configure DYLD_LIBRARY_PATH on Mac
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4366 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- build/dabclogin.sh
+++ build/dabclogin.sh
@@ -6,6 +6,10 @@ export DABCSYS=`pwd`
 
 export PATH=$DABCSYS/bin:$PATH
 
-export LD_LIBRARY_PATH=.:$DABCSYS/lib:$LD_LIBRARY_PATH
+if [ `uname` = "Darwin" ]; then
+   export DYLD_LIBRARY_PATH=.:$DABCSYS/lib:$DYLD_LIBRARY_PATH
+else
+   export LD_LIBRARY_PATH=.:$DABCSYS/lib:$LD_LIBRARY_PATH
+fi
 
 echo Configure DABC version at $DABCSYS
