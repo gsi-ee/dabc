f92d535c869c009b2f4346418dcfff2f60fea78a
Sergey Linev <S.Linev@gsi.de>
2021-04-06 12:01:53 +0000

    Add errorflag in v4 EPOC hldprint
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4756 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/hadaq/hldprint.cxx
+++ plugins/hadaq/hldprint.cxx
@@ -531,7 +531,8 @@ void PrintTdc4Data(hadaq::RawSubevent* sub, unsigned ix, unsigned len, unsigned 
          if (hdr3 == newkind_EPOC) {
             kind = "EPOC";
             unsigned epoch = msg & 0xFFFFFFF;
-            snprintf(sdata, sizeof(sdata), "0x%07x", epoch);
+            bool err = (msg & 0x10000000) != 0;
+            snprintf(sdata, sizeof(sdata), "0x%07x%s", epoch, (err ? " errflag" : ""));
          } else
          if (hdr4 == newkind_TMDS) kind = "TMDS"; else
          if (hdr6 == newkind_TBD) kind = "TBD"; else
