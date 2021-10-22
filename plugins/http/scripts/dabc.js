d9a17db45f0349ab9a490f9aaa7c67dc30a383b1
Sergey Linev <S.Linev@gsi.de>
2021-10-22 16:06:35 +0000

    Update JSROOT, fix dabc.js loading sequence
    Use JSROOT.define for main DABC functionality
    
    git-svn-id: https://subversion.gsi.de/dabc/trunk@4837 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- plugins/http/scripts/dabc.js
+++ plugins/http/scripts/dabc.js
@@ -1,16 +1,18 @@
-JSROOT.require("painter").then(jsrp => {
+const dabcScript = document.currentScript;
+
+JSROOT.define(["painter", "hist"], jsrp => {
 
    "use strict";
 
    JSROOT.settings.DragAndDrop = true;
 
-   let DABC = {};
+   // const jsrp = arr[0];
+
+   const DABC = {};
 
    DABC.version = "2.11.0 22/10/2021";
 
    DABC.source_dir = "";
-
-   const dabcScript = document.currentScript;
 
    if (dabcScript && (typeof dabcScript.src == "string")) {
       const pos = dabcScript.src.indexOf("dabc.js");
@@ -1349,64 +1351,62 @@ JSROOT.require("painter").then(jsrp => {
 
    // ==================================================================
 
-   JSROOT.require('hist').then(() => {
-      JSROOT.TH2Painter.prototype.oldFillHistContextMenu = JSROOT.TH2Painter.prototype.fillHistContextMenu;
+   JSROOT.TH2Painter.prototype.oldFillHistContextMenu = JSROOT.TH2Painter.prototype.fillHistContextMenu;
 
-      JSROOT.TH2Painter.prototype.fillHistContextMenu = function(menu) {
-         let itemname = this.getItemName();
-         if ((typeof itemname == "string") && (itemname.indexOf("HLD_ToTPerChannel") >= 0)) {
-            let tip = menu.painter.getToolTip(menu.getEventPosition());
+   JSROOT.TH2Painter.prototype.fillHistContextMenu = function(menu) {
+      let itemname = this.getItemName();
+      if ((typeof itemname == "string") && (itemname.indexOf("HLD_ToTPerChannel") >= 0)) {
+         let tip = menu.painter.getToolTip(menu.getEventPosition());
 
-            // example how to get label from bin index
-            // if (tip.binx !== undefined) console.log('binx as text', menu.painter.getFramePainter().axisAsText("x", tip.binx));
-            // if (tip.biny !== undefined) console.log('biny as text', menu.painter.getFramePainter().axisAsText("y", tip.biny));
+         // example how to get label from bin index
+         // if (tip.binx !== undefined) console.log('binx as text', menu.painter.getFramePainter().axisAsText("x", tip.binx));
+         // if (tip.biny !== undefined) console.log('biny as text', menu.painter.getFramePainter().axisAsText("y", tip.biny));
 
-            let histo = menu.painter.getHisto(),
-                binlbl = menu.painter.getAxisBinTip("x", histo.fXaxis, tip.binx-1);
+         let histo = menu.painter.getHisto(),
+             binlbl = menu.painter.getAxisBinTip("x", histo.fXaxis, tip.binx-1);
 
-            let focusOnTdc = lbl => {
-               let tdc_name = "TDC_" + lbl.substr(2), tdc_item;
+         let focusOnTdc = lbl => {
+            let tdc_name = "TDC_" + lbl.substr(2), tdc_item;
 
-               JSROOT.hpainter.forEachItem(item => {
-                  if (item._name == tdc_name)
-                     tdc_item = item;
-               });
-               if (tdc_item)
-                  JSROOT.hpainter.focusOnItem(tdc_item);
+            JSROOT.hpainter.forEachItem(item => {
+               if (item._name == tdc_name)
+                  tdc_item = item;
+            });
+            if (tdc_item)
+               JSROOT.hpainter.focusOnItem(tdc_item);
 
-            }
-
-            if (binlbl && (typeof binlbl == "string") && (binlbl.indexOf("0x")==0))
-               menu.add(`Focus on TDC ${binlbl}`, () => focusOnTdc(binlbl));
-
-            menu.add("Find TDC", () => menu.input("TDC id", binlbl, "TDC id").then(id => {
-               if (!id) return;
-               id = id.substr(0,2) + id.substr(2).toUpperCase();
-
-               let nbins = histo.fXaxis.fNbins;
-               for (let bin = 0; bin < nbins; ++bin) {
-                  let lbl = menu.painter.getAxisBinTip("x", histo.fXaxis, bin);
-                  if (lbl == id) {
-                     console.log(`Find id ${id} with bin ${bin}`);
-                     focusOnTdc(id);
-                     let fp = menu.painter.getFramePainter();
-                     fp.zoomSingle("x", Math.max(1, bin - 10), Math.min(nbins-1, bin+10));
-
-                     fp.zoomChangedInteractive("x", true);
-                     break;
-                  }
-               }
-            }));
-
-            // menu.add(`sub:Histogram bin [${tip.binx}, ${tip.biny}]`, () => menu.painter.provideSpecialDrawArea());
-            // menu.add("Show hpx", () => menu.painter.provideSpecialDrawArea("bottom").then(() => hh.getObject("hpx")).then(res => menu.painter.drawInSpecialArea(res.obj, "*H")));
-            // menu.add("Show hprof", () => menu.painter.provideSpecialDrawArea("left").then(() => hh.getObject("hprof")).then(res => menu.painter.drawInSpecialArea(res.obj, "E")));
-            // menu.add("Close extra area", () => menu.painter.provideSpecialDrawArea());
-            // menu.add("endsub:");
          }
-         return this.oldFillHistContextMenu(menu);
-      };
-   });
+
+         if (binlbl && (typeof binlbl == "string") && (binlbl.indexOf("0x")==0))
+            menu.add(`Focus on TDC ${binlbl}`, () => focusOnTdc(binlbl));
+
+         menu.add("Find TDC", () => menu.input("TDC id", binlbl, "TDC id").then(id => {
+            if (!id) return;
+            id = id.substr(0,2) + id.substr(2).toUpperCase();
+
+            let nbins = histo.fXaxis.fNbins;
+            for (let bin = 0; bin < nbins; ++bin) {
+               let lbl = menu.painter.getAxisBinTip("x", histo.fXaxis, bin);
+               if (lbl == id) {
+                  console.log(`Find id ${id} with bin ${bin}`);
+                  focusOnTdc(id);
+                  let fp = menu.painter.getFramePainter();
+                  fp.zoomSingle("x", Math.max(1, bin - 10), Math.min(nbins-1, bin+10));
+
+                  fp.zoomChangedInteractive("x", true);
+                  break;
+               }
+            }
+         }));
+
+         // menu.add(`sub:Histogram bin [${tip.binx}, ${tip.biny}]`, () => menu.painter.provideSpecialDrawArea());
+         // menu.add("Show hpx", () => menu.painter.provideSpecialDrawArea("bottom").then(() => hh.getObject("hpx")).then(res => menu.painter.drawInSpecialArea(res.obj, "*H")));
+         // menu.add("Show hprof", () => menu.painter.provideSpecialDrawArea("left").then(() => hh.getObject("hprof")).then(res => menu.painter.drawInSpecialArea(res.obj, "E")));
+         // menu.add("Close extra area", () => menu.painter.provideSpecialDrawArea());
+         // menu.add("endsub:");
+      }
+      return this.oldFillHistContextMenu(menu);
+   };
 
    // ===================================================================
 
