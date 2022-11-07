f406d09176315751fea152d1b3a69af3daaf3056
hadaq (lxhadeb08) <j.adamczewski@gsi.de>
2022-11-07 12:37:18 +0100

    more adjustments for ltsm in cmake. it builds now

--- cmake/modules/FindLTSM.cmake
+++ cmake/modules/FindLTSM.cmake
@@ -2,14 +2,17 @@
 
 set(LTSM_FOUND 0)
 
-find_library(LTSM_LIBRARY NAMES ApiTSM64 DOC "Searching LTSM library")
+find_library(LTSM_LIBRARY NAMES libltsmapi.a  PATHS /home/hadaq/ltsm/install/lib/ DOC "Searching LTSM library")
 
-find_path(LTSM_INCLUDE_DIR tsmapi.h /usr/include /home/hadaq/ltsm/install/include)
+find_path(LTSM_INCLUDE_DIR ltsmapi.h /usr/include /home/hadaq/ltsm/install/include)
 
-if (LTSM_LIBRARY AND LTSM_INCLUDE_DIR)
+find_path(TSM_INCLUDE_DIR dsmapitd.h /usr/include /opt/tivoli/tsm/client/api/bin64/sample)
+
+
+if (LTSM_LIBRARY AND LTSM_INCLUDE_DIR AND TSM_INCLUDE_DIR)
   set(LTSM_FOUND 1)
-  message(STATUS "Found LTSM includes ${LTSM_INCLUDE_DIR} and library ${LTSM_LIBRARY}")
+  message(STATUS "Found LTSM includes ${LTSM_INCLUDE_DIR} ${TSM_INCLUDE_DIR} and library ${LTSM_LIBRARY}")
 endif()
 
-mark_as_advanced(LTSM_FOUND LTSM_LIBRARY LTSM_INCLUDE_DIR)
+mark_as_advanced(LTSM_FOUND LTSM_LIBRARY LTSM_INCLUDE_DIR TSM_INCLUDE_DIR)
 
