d77f4e23177986d390e2d1e7f0a2a269de5123fd
J. Adamczewski-Musch <j.adamczewski@gsi.de>
2022-12-12 13:58:39 +0100

    gosip plugin: implementation moved to gosip::Command class and TerminaModule

--- cmake/modules/FindGOSIP.cmake
+++ cmake/modules/FindGOSIP.cmake
@@ -2,7 +2,7 @@
 
 set(GOSIP_FOUND 0)
 
-#if(NOT "$ENV{GSI_OS_VERSION}" STREQUAL "")
+if(NOT "$ENV{GSI_OS_VERSION}" STREQUAL "")
 	set(GOSIPHOME /mbs/driv/mbspex_$ENV{GSI_OS_VERSION}_DEB)
 	# build on debian10 gsi linux:
     #set(GOSIPHOME /lynx/Lynx/daq/usr/adamczew/workspace/drivers/mbspex)
@@ -13,7 +13,7 @@ set(GOSIP_FOUND 0)
       set(GOSIP_FOUND 1)
       message(STATUS "Found mbspex includes ${GOSIP_INCLUDE_DIR} and library ${GOSIP_LIBRARY}")
    	endif()
-#endif()
+endif()
 
 mark_as_advanced(GOSIP_FOUND GOSIP_LIBRARY GOSIP_INCLUDE_DIR)
 
