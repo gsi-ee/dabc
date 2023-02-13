9d9941f0b87b4d56926bd5d09dfc922d06901698
J. Adamczewski-Musch <j.adamczewski@gsi.de>
2023-02-13 13:50:32 +0100

    Add cmake script to find dofi includes

---
+++ cmake/modules/FindDOFI.cmake
@@ -0,0 +1,13 @@
+# Find SPI includes required for DOFI
+
+set(DOFI_FOUND 0)
+
+find_path(DOFI_INCLUDE_DIR spi/spidev.h /usr/include/linux/)
+
+    if (DOFI_INCLUDE_DIR)
+      set(DOFI_FOUND 1)
+      message(STATUS "Found SPI includes ${DOFI_INCLUDE_DIR} ")
+   	endif()
+
+mark_as_advanced(DOFI_FOUND DOFI_INCLUDE_DIR)
+
