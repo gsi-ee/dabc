1c7850e997758586cab23361a66f47e224a935a3
J. Adamczewski-Musch <j.adamczewski@gsi.de>
2023-03-28 10:28:53 +0200

    adjust ltsm plugin for new releases of fsq and ltsm

--- cmake/modules/FindLTSM.cmake
+++ cmake/modules/FindLTSM.cmake
@@ -2,23 +2,25 @@
 
 set(LTSM_FOUND 0)
 
-#find_library(LTSM_LIBRARY NAMES libltsmapi.so PATHS /home/hadaq/ltsm/install/lib/ DOC "Searching LTSM library")
-find_library(LTSM_LIBRARY NAMES libltsmapi.so PATHS /mbs/storage/PCx86_Linux_5.10-64_Deb/lib DOC "Searching LTSM library")
+find_library(LTSM_LIBRARY NAMES libltsmapi.so PATHS /home/hadaq/ltsm/install/lib/ DOC "Searching LTSM library")
+#find_library(LTSM_LIBRARY NAMES libltsmapi.so PATHS /mbs/storage/PCx86_Linux_5.10-64_Deb/lib DOC "Searching LTSM library")
 
-#find_library(FSQ_LIBRARY NAMES libfsqapi.so  PATHS /home/hadaq/ltsm/install/lib/ DOC "Searching FSQ library")
-find_library(FSQ_LIBRARY NAMES libfsqapi.so  PATHS /mbs/storage/PCx86_Linux_5.10-64_Deb/lib DOC "Searching FSQ library")
+find_library(FSQ_LIBRARY NAMES libfsqapi.so  PATHS /home/hadaq/ltsm/install/lib/ DOC "Searching FSQ library")
+#find_library(FSQ_LIBRARY NAMES libfsqapi.so  PATHS /mbs/storage/PCx86_Linux_5.10-64_Deb/lib DOC "Searching FSQ library")
 
 
-#find_path(LTSM_INCLUDE_DIR ltsmapi.h /usr/include /home/hadaq/ltsm/install/include)
-find_path(LTSM_INCLUDE_DIR ltsmapi.h /mbs/storage/PCx86_Linux_5.10-64_Deb/include)
+find_path(LTSM_INCLUDE_DIR ltsmapi.h /usr/include/ltsm /home/hadaq/ltsm/install/include/ltsm)
+#find_path(LTSM_INCLUDE_DIR ltsmapi.h /mbs/storage/PCx86_Linux_5.10-64_Deb/include)
+
+find_path(FSQ_INCLUDE_DIR fsqapi.h /usr/include/fsq /home/hadaq/ltsm/install/include/fsq)
 
 find_path(TSM_INCLUDE_DIR dsmapitd.h /usr/include /opt/tivoli/tsm/client/api/bin64/sample)
 #find_path(TSM_INCLUDE_DIR dsmapitd.h /mbs/storage/PCx86_Linux_5.10-64_Deb/include)
 
-if (LTSM_LIBRARY AND FSQ_LIBRARY AND LTSM_INCLUDE_DIR AND TSM_INCLUDE_DIR)
+if (LTSM_LIBRARY AND FSQ_LIBRARY AND LTSM_INCLUDE_DIR AND FSQ_INCLUDE_DIR AND TSM_INCLUDE_DIR)
   set(LTSM_FOUND 1)
-  message(STATUS "Found LTSM includes ${LTSM_INCLUDE_DIR} ${TSM_INCLUDE_DIR} and libraries ${LTSM_LIBRARY} ${FSQ_LIBRARY}")
+  message(STATUS "Found LTSM includes ${LTSM_INCLUDE_DIR} ${FSQ_INCLUDE_DIR} ${TSM_INCLUDE_DIR} and libraries ${LTSM_LIBRARY} ${FSQ_LIBRARY}")
 endif()
 
-mark_as_advanced(LTSM_FOUND LTSM_LIBRARY FSQ_LIBRARY LTSM_INCLUDE_DIR TSM_INCLUDE_DIR)
+mark_as_advanced(LTSM_FOUND LTSM_LIBRARY FSQ_LIBRARY LTSM_INCLUDE_DIR FSQ_INCLUDE_DIR TSM_INCLUDE_DIR)
 
