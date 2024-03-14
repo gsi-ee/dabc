06437ca513d440f8bb8428a6cce5a1e7b2d8a08f
Sergey Linev <S.Linev@gsi.de>
2024-03-14 13:21:59 +0100

    Fix warnings in dofi

--- plugins/dofi/src/TerminalModule.cxx
+++ plugins/dofi/src/TerminalModule.cxx
@@ -45,16 +45,16 @@ dofi::TerminalModule::TerminalModule (const std::string &name, dabc::Command cmd
 	fSPI_Device = Cfg("SPI_Device", cmd).AsStr(DOFI_SPIDEV_DEFAULT); // may change these parameters in dabc xml config file
 	fSPI_mode =Cfg("SPI_Mode", cmd).AsInt(0);
 	fSPI_bits  =Cfg("SPI_Bits", cmd).AsInt(8);
-	fSPI_speed=Cfg("SPI_Speed", cmd).AsInt(20000000);	
-	
-	
+	fSPI_speed=Cfg("SPI_Speed", cmd).AsInt(20000000);
+
+
 	// below definitions for web server:
-	
+
    fWorkerHierarchy.Create("DOFI", true);
 
    // web client has same command structure as dabc socket command:1
    //dabc::CommandDefinition cmddef = CreateCmdDef("DofiCommand");
-   
+
    dabc::CommandDefinition cmddef = fWorkerHierarchy.CreateHChild("DofiCommand");
    cmddef.SetField(dabc::prop_kind, "DABC.Command");
     //cmddef.SetField(dabc::prop_auth, true); // require authentication
@@ -65,7 +65,7 @@ dofi::TerminalModule::TerminalModule (const std::string &name, dabc::Command cmd
    cmddef.AddArg("VALUE", "uint", true, 0);
    cmddef.AddArg("REPEAT", "int", true, 1);
    cmddef.AddArg("FILENAME", "string", true, 1);
-   
+
    dabc::Hierarchy ui = fWorkerHierarchy.CreateHChild("UI");
    ui.SetField(dabc::prop_kind, "DABC.HTML");
    ui.SetField("_UserFilePath", "${DABCSYS}/plugins/dofi/htm/");
@@ -74,9 +74,9 @@ dofi::TerminalModule::TerminalModule (const std::string &name, dabc::Command cmd
    CreateTimer("update", 1.);
    //Publish(fWorkerHierarchy, std::string("/Control/") + fSPI_Device);
    PublishPars("dofi");
-	
-	
-	
+
+
+
 }
 
 dofi::TerminalModule::~TerminalModule()
@@ -87,15 +87,15 @@ dofi::TerminalModule::~TerminalModule()
 int dofi::TerminalModule::ExecuteCommand (dabc::Command cmd)
 {
   int l_status = 0;
-  
+
    DOUT2("dofi::TerminalModule::ExecuteCommand for %s",cmd.GetName());
-   
-    
-   
-   
+
+
+
+
   if (cmd.IsName ("DofiCommand") || cmd.IsName(dabc::CmdHierarchyExec::CmdName()))
   {
-   
+
     if(cmd.IsName(dabc::CmdHierarchyExec::CmdName()))
     {
 		 std::string cmdpath = cmd.GetStr("Item");
@@ -103,8 +103,8 @@ int dofi::TerminalModule::ExecuteCommand (dabc::Command cmd)
 		 // here suppress wrong hierarchy commmands
 		if(cmdpath.compare("DofiCommand")!=0) return dabc::cmd_false;
 	}
-   
-   
+
+
     dofi::Command theCommand;
     theCommand.ExtractDabcCommand (cmd);
     if (theCommand.verboselevel > 1)
@@ -152,7 +152,7 @@ void dofi::TerminalModule::ProcessTimerEvent (unsigned)
 
 int dofi::TerminalModule::open_configuration (dofi::Command &com)
 {
-// JAM 10-02-23 - code taken from gosip plugin and adjusted to dofi	
+// JAM 10-02-23 - code taken from gosip plugin and adjusted to dofi
 // JAM 12-12-22: note that configuration file is on command server file system
 // TODO: may later provide mechanism for local config file on rgoc client host which may be send
 // embedded into dabc Command and evaluated in execute command (via temporary file?)
@@ -270,30 +270,30 @@ int dofi::TerminalModule::next_config_values (dofi::Command &com)
 }
 
 int dofi::TerminalModule::write (dofi::Command &com)
-{  
-  DOFI_ASSERT_DEVHANDLE;  
+{
+  DOFI_ASSERT_DEVHANDLE;
   uint16_t address=(com.address & 0xFFFF);
-  uint64_t data=com.value;  
+  uint64_t data=com.value;
   try
   {
     SPI_write(address, data);
     return 0;
   }
   catch(dabc::Exception &ex)
-  {  
+  {
     com.dump_command ();
     std::stringstream hexstream;
     hexstream << std::hex << com.address; // no better way to do hex formatting with std::string?  JAM :-(
     std::string text="ERROR on writing to address: 0x" + hexstream.str() +": ";
     text += ex.ItemName() + " - "+ ex.what() + "\n";
-    
+
     if (fCommandMessage.compare("OK")==0)
       fCommandMessage=text;
     else
-      fCommandMessage+=text; 
-    
-    EOUT(text.c_str()); // for local server log
-  	com.dump_command ();    
+      fCommandMessage+=text;
+
+    EOUT("%s", text.c_str()); // for local server log
+  	com.dump_command ();
     return -1;   // TODO: throw further here?
     }
   return 0;
@@ -301,15 +301,15 @@ int dofi::TerminalModule::write (dofi::Command &com)
 
 int dofi::TerminalModule::read (dofi::Command &com)
 {
-  DOFI_ASSERT_DEVHANDLE;  
-  uint16_t address=(com.address & 0xFFFF); 
+  DOFI_ASSERT_DEVHANDLE;
+  uint16_t address=(com.address & 0xFFFF);
   try
   {
      com.value=SPI_read(address);
   	 return 0;
    }
   catch(dabc::Exception &ex)
-  {    
+  {
     std::stringstream hexstream;
     hexstream << std::hex << com.address; // no better way to do hex formatting with std::string?  JAM :-(
     std::string text="ERROR on reading from address: 0x" + hexstream.str() +": ";
@@ -317,12 +317,12 @@ int dofi::TerminalModule::read (dofi::Command &com)
     if (fCommandMessage.compare("OK")==0)
       fCommandMessage=text;
     else
-      fCommandMessage+=text; 
-  	EOUT(text.c_str()); // for local server log
+      fCommandMessage+=text;
+  	EOUT("%s", text.c_str()); // for local server log
   	com.dump_command ();
   return -1; // TODO: throw further here?
   }
- 
+
   return 0;
 }
 
@@ -413,14 +413,14 @@ int  dofi::TerminalModule::reset (dofi::Command &com)
   	SPI_open_device();
   }
    catch(dabc::Exception &ex)
-   {    
+   {
     std::string text="Error on resetting dofi SPI defice: ";
 	text += ex.ItemName() + " - "+ ex.what() + "\n";
       fCommandMessage=text;
-  	EOUT(text.c_str()); // for local server log
+  	EOUT("%s", text.c_str()); // for local server log
   	com.dump_command ();
   return -1; // TODO: throw further here?
-  }  
+  }
   fCommandMessage= ("Resetted dofi SPI device.");
   return 0;
 }
@@ -452,7 +452,7 @@ int dofi::TerminalModule::configure (dofi::Command &com)
 			else
 			{
 			  if ((rev=this->write (com)) != 0)
-			  
+
 			  break;
 			}
 
@@ -460,18 +460,18 @@ int dofi::TerminalModule::configure (dofi::Command &com)
 
   }// try
   catch(dabc::Exception &ex)
-  {    
-    std::string text="ERROR during configuration from file"; 
+  {
+    std::string text="ERROR during configuration from file";
     text += com.filename;
-    text += ex.ItemName(); 
+    text += ex.ItemName();
     text += " - ";
     text += ex.what();
     text += "\n";
     if (fCommandMessage.compare("OK")==0)
       fCommandMessage=text;
     else
-      fCommandMessage+=text; 
-  	EOUT(text.c_str()); // for local server log
+      fCommandMessage+=text;
+  	EOUT("%s", text.c_str()); // for local server log
   	com.dump_command ();
   return -1; // TODO: throw further here?
   }
@@ -502,17 +502,17 @@ int dofi::TerminalModule::single_command (dofi::Command &com)
   	default:
   		EOUT("NEVER COME HERE: dofi::TerminalModule::single_command with not suitable command %d (%s)", com.command, com.get_description());
   	return -1;
-  	break;	 
-  		 
+  	break;
+
   }
-		
+
 try
   {
      SPI_single_command(token);
   	 return 0;
    }
   catch(dabc::Exception &ex)
-  {    
+  {
     std::string text="ERROR on command -";
     text += com.get_description();
     text += " - ";
@@ -520,8 +520,8 @@ try
     if (fCommandMessage.compare("OK")==0)
       fCommandMessage=text;
     else
-      fCommandMessage+=text; 
-  	EOUT(text.c_str()); // for local server log
+      fCommandMessage+=text;
+  	EOUT("%s", text.c_str()); // for local server log
   	com.dump_command ();
   return -1; // TODO: throw further here?
   }
@@ -548,11 +548,11 @@ int dofi::TerminalModule::execute_command (dofi::Command &com)
     case DOFI_CLEARBIT:
       rev = busio (com);
       break;
-      
+
     case DOFI_CONFIGURE:
       rev = configure (com);
       break;
-      
+
     case dofi::DOFI_RESET_SCALER:
     case dofi::DOFI_ENABLE_SCALER:
  	case dofi::DOFI_DISABLE_SCALER:
@@ -575,7 +575,7 @@ int dofi::TerminalModule::execute_command (dofi::Command &com)
 
 int dofi::TerminalModule::SPI_open_device ()
 {
-  int ret=0;	
+  int ret=0;
   if(fFD_spi) return -2;
   DOUT0("Opening spi handle..");
   fFD_spi = open(fSPI_Device.c_str(), O_RDWR);
@@ -584,15 +584,15 @@ int dofi::TerminalModule::SPI_open_device ()
     EOUT ("ERROR>> open %s\n", fSPI_Device.c_str());
     throw dabc::Exception("Error when opening handle!", "dofi::TerminalModule::open_device() ");
     }
-  
-  
+
+
   ret = ioctl(fFD_spi, SPI_IOC_WR_MODE, &fSPI_mode);
   if (ret == -1 ) throw dabc::Exception("Can't set spi mode", "dofi::TerminalModule::open_device() ");
 
 
   ret = ioctl(fFD_spi, SPI_IOC_RD_MODE, &fSPI_mode);
   if (ret == -1) throw dabc::Exception("Can't get spi mode", "dofi::TerminalModule::open_device() ");
- 
+
    ret = ioctl(fFD_spi, SPI_IOC_WR_BITS_PER_WORD, &fSPI_bits);
    if (ret == -1) throw dabc::Exception("Can't set bits per word", "dofi::TerminalModule::open_device() ");
 
@@ -604,12 +604,12 @@ int dofi::TerminalModule::SPI_open_device ()
 
    ret = ioctl(fFD_spi, SPI_IOC_RD_MAX_SPEED_HZ, &fSPI_speed);
    if (ret == -1) throw dabc::Exception("Can't get max speed Hz", "dofi::TerminalModule::open_device() ");
-   
+
   fSPI_transfer.tx_buf = (unsigned long) fSPI_tx;
   fSPI_transfer.rx_buf = (unsigned long) fSPI_rx;
   fSPI_transfer.speed_hz      = fSPI_speed;
   fSPI_transfer.bits_per_word = fSPI_bits;
-  
+
   DOUT0("Initialized spi device %s", fSPI_Device.c_str());
   DOUT0(" - spi mode: \t%d", fSPI_mode);
   DOUT0(" - bits per word: \t%d", fSPI_bits);
@@ -624,7 +624,7 @@ int dofi::TerminalModule::SPI_open_device ()
 
 int dofi::TerminalModule::SPI_write (uint16_t l_addrx, uint64_t l_wr_datx)
 {
-  int ret=0; 	
+  int ret=0;
   fSPI_tx[10] = (dofi::TerminalModule::DOFI_SPI_WRITE & 0xff);  // 0x01 write to slave
   fSPI_tx[9]  = ( l_addrx         & 0xff);   	// register address, low  8 bits
   fSPI_tx[8]  = ((l_addrx >> 8)   & 0xff);   	// register address, high 8 bits
@@ -638,7 +638,7 @@ int dofi::TerminalModule::SPI_write (uint16_t l_addrx, uint64_t l_wr_datx)
   fSPI_tx[0]  = (l_wr_datx >> 56) & 0xff;
   fSPI_transfer.len = 11;
 
-  DOUT5("0x%x fSPI_tx  ", l_addrx); 
+  DOUT5("0x%x fSPI_tx  ", l_addrx);
   for (unsigned int i = 0; i < fSPI_transfer.len; i++)
   {
     DOUT5("%.2X ", fSPI_tx[i]);
@@ -647,7 +647,7 @@ int dofi::TerminalModule::SPI_write (uint16_t l_addrx, uint64_t l_wr_datx)
 
 
   ret = ioctl(fFD_spi, SPI_IOC_MESSAGE(1), &fSPI_transfer);
-  if (ret < 1)   
+  if (ret < 1)
   {
   	std::string message="Can't send ioctl message to spi kernel module " + fSPI_Device;
 	throw dabc::Exception(message, "dofi::TerminalModule::SPI_write() ");
@@ -660,12 +660,12 @@ int dofi::TerminalModule::SPI_write (uint16_t l_addrx, uint64_t l_wr_datx)
 
 uint64_t dofi::TerminalModule::SPI_read (uint16_t l_addrx)
 {
-  int ret=0; 	
+  int ret=0;
   uint64_t l_rd_datx, l_rd_datx_tmp;
-  
+
   fSPI_tx[10] = (DOFI_SPI_READ & 0xff); //0x02; read from slave
   fSPI_tx[9]  = ( l_addrx        & 0xff);   // register address, low  8 bits
-  fSPI_tx[8]  = ((l_addrx >> 8)  & 0xff);   // register address, high 8 bits  
+  fSPI_tx[8]  = ((l_addrx >> 8)  & 0xff);   // register address, high 8 bits
   fSPI_tx[7]  = 0;
   fSPI_tx[6]  = 0;
   fSPI_tx[5]  = 0;
@@ -675,9 +675,9 @@ uint64_t dofi::TerminalModule::SPI_read (uint16_t l_addrx)
   fSPI_tx[1]  = 0;
   fSPI_tx[0]  = 0;
   fSPI_transfer.len = 11;
-      
+
   ret = ioctl(fFD_spi, SPI_IOC_MESSAGE(1), &fSPI_transfer);
-  if (ret < 1) 
+  if (ret < 1)
   	{
   	std::string message="Can't send first ioctl message to spi kernel module " + fSPI_Device;
 	throw dabc::Exception(message, "dofi::TerminalModule::SPI_read() ");
@@ -694,23 +694,23 @@ uint64_t dofi::TerminalModule::SPI_read (uint16_t l_addrx)
   fSPI_tx[1]  = 0;
   fSPI_tx[0]  = 0;
   fSPI_transfer.len = 11;
-      
+
   ret = ioctl(fFD_spi, SPI_IOC_MESSAGE(1), &fSPI_transfer);
-  if (ret < 1) 
+  if (ret < 1)
   {
 	  std::string message="Can't send second ioctl message to spi kernel module ";
 	  message += fSPI_Device;
 	  throw dabc::Exception(message, "dofi::TerminalModule::SPI_read() ");
 	}
 
- 	
-  DOUT5("0x%x rx  ", l_addrx); 
+
+  DOUT5("0x%x rx  ", l_addrx);
   for (unsigned int i = 0; i < fSPI_transfer.len; i++)
   {
     DOUT5("%.2X ", fSPI_rx[i]);
   }
   DOUT5("\n");
-  
+
   l_rd_datx = 0;
   l_rd_datx_tmp = fSPI_rx[3]; l_rd_datx_tmp <<= 56; l_rd_datx += l_rd_datx_tmp;
   l_rd_datx_tmp = fSPI_rx[4]; l_rd_datx_tmp <<= 48; l_rd_datx += l_rd_datx_tmp;
@@ -721,7 +721,7 @@ uint64_t dofi::TerminalModule::SPI_read (uint16_t l_addrx)
   l_rd_datx_tmp = fSPI_rx[9]; l_rd_datx_tmp <<=  8; l_rd_datx += l_rd_datx_tmp;
   l_rd_datx_tmp = fSPI_rx[10];                      l_rd_datx += l_rd_datx_tmp;
 
-  DOUT3("spi  read: addr: 0x%x, data: 0x%llx \n", l_addrx, l_rd_datx);    
+  DOUT3("spi  read: addr: 0x%x, data: 0x%llx \n", l_addrx, l_rd_datx);
   return l_rd_datx;
 }
 
@@ -744,15 +744,15 @@ int dofi::TerminalModule::SPI_single_command (spi_cmd_token token)
   fSPI_tx[0]  = 0;
   fSPI_transfer.len = 11;
 
-  DOUT5("0x%x fSPI_tx  ", l_addrx); 
+  DOUT5("0x%x fSPI_tx  ", l_addrx);
   for (unsigned int i = 0; i < fSPI_transfer.len; i++)
   {
     DOUT5("%.2X ", fSPI_tx[i]);
   }
   DOUT5("\n");
-  
+
   int ret = ioctl(fFD_spi, SPI_IOC_MESSAGE(1), &fSPI_transfer);
-  if (ret < 1) 
+  if (ret < 1)
   	{
 	  std::string message="Can't send ioctl message to spi kernel module ";
 	  message += fSPI_Device;
