7ad58083b80a4cb564456c534be6f56de0282d1a
Sergey Linev <S.Linev@gsi.de>
2024-03-15 09:15:14 +0100

    Add api.h with readout module to dogma

---
+++ plugins/dogma/dogma/api.h
@@ -0,0 +1,69 @@
+// $Id$
+
+/************************************************************
+ * The Data Acquisition Backbone Core (DABC)                *
+ ************************************************************
+ * Copyright (C) 2009 -                                     *
+ * GSI Helmholtzzentrum fuer Schwerionenforschung GmbH      *
+ * Planckstr. 1, 64291 Darmstadt, Germany                   *
+ * Contact:  http://dabc.gsi.de                             *
+ ************************************************************
+ * This software can be used under the GPL license          *
+ * agreements as stated in LICENSE.txt file                 *
+ * which is part of the distribution.                       *
+ ************************************************************/
+
+#ifndef DOGMA_api
+#define DOGMA_api
+
+#ifndef MBS_api
+#include "mbs/api.h"
+#endif
+
+#ifndef DOGMA_Iterator
+#include "dogma/Iterator.h"
+#endif
+
+namespace dogma {
+
+   class ReadoutHandle;
+
+   class ReadoutModule : public mbs::ReadoutModule {
+      protected:
+
+         friend class ReadoutHandle;
+
+         dogma::RawIterator fIter2;   ///< iterator over DOGMA buffers
+
+         int AcceptBuffer(dabc::Buffer &buf) override;
+
+      public:
+
+         ReadoutModule(const std::string &name, dabc::Command cmd);
+   };
+
+
+   class ReadoutHandle : protected mbs::ReadoutHandle {
+
+      DABC_REFERENCE(ReadoutHandle, mbs::ReadoutHandle, dogma::ReadoutModule)
+
+      /** Connect with data source */
+      static ReadoutHandle Connect(const std::string &url);
+
+      /** Return true if handle not initialized */
+      bool null() const { return mbs::ReadoutHandle::null(); }
+
+      /** Disconnect from MBS server */
+      bool Disconnect() { return mbs::ReadoutHandle::Disconnect(); }
+
+      /** Retrieve next event from the server */
+      DogmaTu *NextTu(double tm = 1.0, double maxage = -1.);
+
+      /** Get current event pointer */
+      DogmaTu *GetTu();
+   };
+
+}
+
+
+#endif
