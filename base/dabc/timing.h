54d22f65a2632a78ccc290a0b8bcc9365ea1056c
Sergey Linev <S.Linev@gsi.de>
2024-07-03 12:56:48 +0200

    Format some timing.h lines

--- base/dabc/timing.h
+++ base/dabc/timing.h
@@ -108,13 +108,13 @@ namespace dabc {
 
          TimeStamp& operator=(const TimeStamp& src) { fValue = src.fValue; return *this; }
 
-         TimeStamp& operator+=(double _add) { fValue+=_add; return *this; }
+         TimeStamp& operator+=(double _add) { fValue += _add; return *this; }
 
-         TimeStamp& operator-=(double _sub) { fValue-=_sub; return *this; }
+         TimeStamp& operator-=(double _sub) { fValue -= _sub; return *this; }
 
-         TimeStamp operator+(double _add) const { return TimeStamp(fValue+_add); }
+         TimeStamp operator+(double _add) const { return TimeStamp(fValue + _add); }
 
-         TimeStamp operator-(double _sub) const { return TimeStamp(fValue-_sub); }
+         TimeStamp operator-(double _sub) const { return TimeStamp(fValue - _sub); }
 
          double operator()() const { return fValue; }
 
@@ -170,7 +170,7 @@ namespace dabc {
           * \param[in] curr      current time
           * \param[in] interval  time which should expire
           * \returns             true if specified time interval had expired  */
-         inline bool Expired(const TimeStamp& curr, double interval) const { return curr.AsDouble() > AsDouble() + interval; }
+         inline bool Expired(const TimeStamp &curr, double interval) const { return curr.AsDouble() > AsDouble() + interval; }
 
          /** \brief Method returns TimeStamp instance with current time stamp value, measured
           * either by fast TSC (if it is detected and working correctly), otherwise slow getclock() method
