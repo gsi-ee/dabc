dc10900d208aa6267083e578f87a00d18610cd83
Sergey Linev <S.Linev@gsi.de>
2025-05-27 13:27:41 +0200

    Use better swap approach
    Do not check every time if data swapped or not - just use as is.

--- plugins/dogma/dogma/defines.h
+++ plugins/dogma/dogma/defines.h
@@ -27,6 +27,8 @@
 
 #define SWAP32(v)  (((v & 0xFF) << 24) | ((v & 0xFF00) << 8) | ((v & 0xFF0000) >> 8) | ((v & 0xFF000000) >> 24))
 
+#define SWAP_VALUE(v) (((v & 0xFF) << 24) | ((v & 0xFF00) << 8) | ((v & 0xFF0000) >> 8) | ((v & 0xFF000000) >> 24))
+
 namespace dogma {
 
    /** \brief DOGMA transport unit header
@@ -44,6 +46,7 @@ namespace dogma {
          uint32_t tuLocalTrigTime = 0;
          uint32_t tuLenPayload = 0; // number of 4bytes words
 
+         /*
          inline uint32_t Value(const uint32_t *member) const
          {
             return IsSwapped() ? __bswap_32(*member) : *member;
@@ -53,43 +56,49 @@ namespace dogma {
          {
             *member = IsSwapped() ? __bswap_32(value) : value;
          }
+         */
 
       public:
 
          inline bool IsSwapped() const  { return tuMagic != DOGMA_MAGIC; }
 
-         inline bool IsMagic() const { return Value(&tuMagic) == DOGMA_MAGIC; }
+         inline bool IsMagic() const { return SWAP_VALUE(tuMagic) == DOGMA_MAGIC; }
 
-         inline uint32_t GetAddr() const { return Value(&tuAddr); }
+         inline uint32_t GetAddr() const { return SWAP_VALUE(tuAddr); }
 
-         inline uint32_t GetTrigType() const { return Value(&tuTrigTypeNumber) >> 24; }
+         inline uint32_t GetTrigType() const { return SWAP_VALUE(tuTrigTypeNumber) >> 24; }
 
-         inline uint32_t GetTrigNumber() const { return Value(&tuTrigTypeNumber) & 0xffffff; }
+         inline uint32_t GetTrigNumber() const { return SWAP_VALUE(tuTrigTypeNumber) & 0xffffff; }
 
-         inline uint32_t GetTrigTypeNumber() const { return Value(&tuTrigTypeNumber); }
+         inline uint32_t GetTrigTypeNumber() const { return SWAP_VALUE(tuTrigTypeNumber); }
 
-         inline uint32_t GetTrigTime() const { return Value(&tuTrigTime); }
+         inline uint32_t GetTrigTime() const { return SWAP_VALUE(tuTrigTime); }
 
-         inline uint32_t GetLocalTrigTime() const { return Value(&tuLocalTrigTime); }
+         inline uint32_t GetLocalTrigTime() const { return SWAP_VALUE(tuLocalTrigTime); }
 
-         inline uint32_t GetPayloadLen() const { return Value(&tuLenPayload) & 0xffff; }
+         inline uint32_t GetPayloadLen() const { return SWAP_VALUE(tuLenPayload) & 0xffff; }
 
-         inline uint32_t GetErrorBits() const { return Value(&tuLenPayload) >> 24; }
+         inline uint32_t GetErrorBits() const { return SWAP_VALUE(tuLenPayload) >> 24; }
 
-         inline uint32_t GetFrameBits() const { return (Value(&tuLenPayload) >> 16) & 0xff; }
+         inline uint32_t GetFrameBits() const { return (SWAP_VALUE(tuLenPayload) >> 16) & 0xff; }
 
-         inline void SetPayloadLen(uint32_t len) { SetValue(&tuLenPayload, (len & 0xffff) | (Value(&tuLenPayload) & 0xffff0000)); }
+         inline void SetPayloadLen(uint32_t len)
+         {
+            uint32_t new_payload = (len & 0xffff) | (SWAP_VALUE(tuLenPayload) & 0xffff0000);
+            tuLenPayload = SWAP_VALUE(new_payload);
+         }
 
          inline uint32_t GetSize() const { return sizeof(DogmaTu) + GetPayloadLen()*4; }
 
-         inline uint32_t GetPayload(uint32_t indx) const { return Value(&tuLenPayload + 1 + indx); }
+         inline uint32_t GetPayload(uint32_t indx) const
+         {
+            uint32_t v = *(&tuLenPayload + 1 + indx);
+            return SWAP_VALUE(v);
+         }
 
          void *RawData() const { return (char *) this + sizeof(DogmaTu); }
 
-         void SetTrigTypeNumber(uint32_t type_number)
-         {
-            SetValue(&tuTrigTypeNumber, type_number);
-         }
+         void SetTrigTypeNumber(uint32_t type_number) { tuTrigTypeNumber = SWAP_VALUE(type_number); }
 
          void Init(uint32_t type_number)
          {
@@ -110,6 +119,7 @@ namespace dogma {
          uint32_t tuTrigTypeNumber = 0;
          uint32_t tuLenPayload = 0; // paylod len in 4bytes words
 
+         /*
          inline uint32_t Value(const uint32_t *member) const
          {
             return IsSwapped() ? __bswap_32(*member) : *member;
@@ -119,26 +129,28 @@ namespace dogma {
          {
             *member = IsSwapped() ? __bswap_32(value) : value;
          }
+         */
 
       public:
 
          inline bool IsSwapped() const  { return tuMagic != DOGMA_MAGIC; }
-         inline bool IsMagic() const { return Value(&tuMagic) == DOGMA_MAGIC; }
+         inline bool IsMagic() const { return SWAP_VALUE(tuMagic) == DOGMA_MAGIC; }
 
-         inline uint32_t GetSeqId() const { return Value(&tuSeqId); }
-         inline uint32_t GetTrigType() const { return Value(&tuTrigTypeNumber) >> 24; }
-         inline uint32_t GetTrigNumber() const { return Value(&tuTrigTypeNumber) & 0xffffff; }
+         inline uint32_t GetSeqId() const { return SWAP_VALUE(tuSeqId); }
+         inline uint32_t GetTrigType() const { return SWAP_VALUE(tuTrigTypeNumber) >> 24; }
+         inline uint32_t GetTrigNumber() const { return SWAP_VALUE(tuTrigTypeNumber) & 0xffffff; }
 
          void Init(uint32_t seqid, uint32_t trig_type, uint32_t trig_number)
          {
             tuMagic = SWAP32(DOGMA_MAGIC);
-            SetValue(&tuSeqId, seqid);
-            SetValue(&tuTrigTypeNumber, (trig_type << 24) | (trig_number & 0xffffff));
+            tuSeqId = SWAP_VALUE(seqid);
+            uint32_t v = (trig_type << 24) | (trig_number & 0xffffff);
+            tuTrigTypeNumber = SWAP_VALUE(v);
             tuLenPayload = 0;
          }
 
-         inline uint32_t GetPayloadLen() const { return Value(&tuLenPayload) & 0xffff; }
-         inline void SetPayloadLen(uint32_t len) { SetValue(&tuLenPayload, len); }
+         inline uint32_t GetPayloadLen() const { return SWAP_VALUE(tuLenPayload) & 0xffff; }
+         inline void SetPayloadLen(uint32_t len) { tuLenPayload = SWAP_VALUE(len); }
 
          inline uint32_t GetEventLen() const { return sizeof(DogmaEvent) + GetPayloadLen() * 4; }
 
