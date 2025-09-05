adb3661336b055dc9b93a0187ca186583ebc5f95
J. Adamczewski-Musch <j.adamczewski@gsi.de>
2025-09-05 10:33:31 +0200

    removed redundant elder libraries from cmake find elder

--- cmake/modules/FindELDER.cmake
+++ cmake/modules/FindELDER.cmake
@@ -6,7 +6,7 @@ set(ELDER_FOUND 0)
 set(ELDERHOME $ENV{ELDERSYS})
 find_library(ELDER_LIBRARY NAMES libelderpt-0.1.so  PATHS ${ELDERHOME}/lib DOC "Searching ELDER library")
 find_library(ELDER-STD_LIBRARY NAMES libelderptstd.so  PATHS ${ELDERHOME}/lib DOC "Searching ELDER-STD library")
-find_library(ELDER-GSI_LIBRARY NAMES libelderptgsi_gs.so  PATHS ${ELDERHOME}/lib DOC "Searching ELDER-GSI library")
+#find_library(ELDER-GSI_LIBRARY NAMES libelderptgsi_gs.so  PATHS ${ELDERHOME}/lib DOC "Searching ELDER-GSI library")
 find_library(ELDER-EEL_LIBRARY NAMES libelderpteel.so  PATHS ${ELDERHOME}/lib DOC "Searching ELDER-EEL library")
 
 
@@ -25,10 +25,10 @@ message(STATUS "Found ELDER std library ${ELDER-STD_LIBRARY}")
 mark_as_advanced(ELDER-STD_LIBRARY)
 endif()
 
-if(ELDER-GSI_LIBRARY)
-message(STATUS "Found ELDER gsi_gs library ${ELDER-GSI_LIBRARY}")
-mark_as_advanced(ELDER-GSI_LIBRARY)
-endif()
+#if(ELDER-GSI_LIBRARY)
+#message(STATUS "Found ELDER gsi_gs library ${ELDER-GSI_LIBRARY}")
+#mark_as_advanced(ELDER-GSI_LIBRARY)
+#endif()
 
 if(ELDER-EEL_LIBRARY)
 message(STATUS "Found ELDER EEL library ${ELDER-EEL_LIBRARY}")
