d3682bf3275fad20f4580836659f6dad89e6da03
Sergey Linev <S.Linev@gsi.de>
2025-11-03 11:23:10 +0100

    Add new tdc5.h from Maxi

--- plugins/dogma/dogma/tdc5.h
+++ plugins/dogma/dogma/tdc5.h
@@ -22,6 +22,7 @@ struct tdc5_parse_it {
 	int is_first;
 	uint64_t last_time;
 	uint8_t block_flags;
+	int finetime_len;
 };
 struct tdc5_header {
 	uint32_t magic;
@@ -29,41 +30,49 @@ struct tdc5_header {
 	uint32_t trig_num;
 	uint32_t device_id;
 	uint64_t trig_time;
+	int finetime_len, tdc_type, freq;
 };
 struct tdc5_time {
 	int channel;
-	uint64_t coarse;
+	uint32_t coarse;
 	uint32_t fine;
 	int is_falling;
 };
 
-inline void tdc5_parse_header(tdc5_header *h, tdc5_parse_it *it, const char *buf, int)
+inline void tdc5_parse_header(tdc5_header *h, tdc5_parse_it *it, const char *buf, int /* n */)
 {
 	it->i = 0;
-	h->magic = be32toh(*(uint32_t*)(buf + it->i)); it->i += 4;
+	h->magic = be16toh(*(uint16_t*)(buf + it->i)); it->i += 2;
+	uint16_t tdcinf = be16toh(*(uint16_t*)(buf + it->i)); it->i += 2;
+	h->finetime_len = tdcinf & 0x1f; tdcinf >>= 5;
+	h->tdc_type = tdcinf & 0x1f; tdcinf >>= 5;
+	h->freq = tdcinf * 10;
 	uint32_t trig_type_num = be32toh(*(uint32_t*)(buf + it->i)); it->i += 4;
-	h->trig_type = trig_type_num >> 24;
-	h->trig_num = trig_type_num & 0x00ffffff;
+	h->trig_type = trig_type_num >> 28;
+	h->trig_num = trig_type_num & 0x0fffffff;
 	h->device_id = be32toh(*(uint32_t*)(buf + it->i)); it->i += 4;
 	h->trig_time = be64toh(*(uint64_t*)(buf + it->i)); it->i += 8;
 	it->i += 4; // currently unused
 	it->cur_chan = 0;
 	it->is_first = 1;
+	it->finetime_len = h->finetime_len;
 }
 inline int tdc5_parse_next(tdc5_time *res, tdc5_parse_it *it, const char *buf, int n) // return codes: negative: error; zero: success; positive: continue
 {
-	do { // repeat until we have a non-empty channel
+	int is_channel_empty;
+	do {
 		if (it->i == n) { // stream finished
 			return it->is_first ? 0 : -1;
 		}
 
+		is_channel_empty = 0;
 		if (it->is_first) { // this is the first word/time in the channel sequence
 			if ((it->cur_chan & 7) == 0) { // ... the first channel in a channel block consisting of 8 channels
 				if (it->i == n) return -1;
 				it->block_flags = buf[it->i++]; // load 8 channel flags
 			}
 			// check if current channel is empty and shift flags s.t. flag of the next channel is the LSB
-			int is_channel_empty = !(it->block_flags & 1);
+			is_channel_empty = !(it->block_flags & 1);
 			it->block_flags >>= 1;
 
 			it->last_time = 0;
@@ -71,34 +80,34 @@ inline int tdc5_parse_next(tdc5_time *res, tdc5_parse_it *it, const char *buf, i
 			if (is_channel_empty) {
 				it->is_first = 1;
 				++it->cur_chan;
-				continue;
 			}
 		}
+	} while (is_channel_empty);
 
-		// decode times
-		if (it->i + 3 > n) return -1;
-		uint16_t msbs = be16toh(*(uint16_t*)(buf + it->i)); it->i += 2;
-		int is_last = !!(msbs & 0x8000);
-		int is_ext = !!(msbs & 0x4000);
-		msbs = msbs & 0x3fff;
-		int lsb_time_len = 1 + is_ext;
-		if (it->i + lsb_time_len > n) return -1;
-		uint16_t lsbs = is_ext ? be16toh(*(uint16_t*)(buf + it->i)) : (uint8_t)buf[it->i]; it->i += lsb_time_len;
-		uint32_t time = (msbs << (lsb_time_len * 8)) | lsbs;
-		res->is_falling = time & 1;
-		res->fine = (time >> 1) & 0x1ff;
-		int32_t diff = time >> 10;
-		it->last_time = it->is_first ? diff : it->last_time - diff;
-		res->coarse = it->last_time;
-		res->channel = it->cur_chan;
+	// decode times
+	if (it->i + 3 > n) return -1;
+	uint16_t msbs = be16toh(*(uint16_t*)(buf + it->i)); it->i += 2;
+	int is_last = !!(msbs & 0x8000);
+	int is_ext = !!(msbs & 0x4000);
+	msbs = msbs & 0x3fff;
+	int lsb_time_len = 1 + is_ext;
+	if (it->i + lsb_time_len > n) return -1;
+	uint16_t lsbs = is_ext ? be16toh(*(uint16_t*)(buf + it->i)) : (uint8_t)buf[it->i]; it->i += lsb_time_len;
+	uint32_t time = (((uint32_t)msbs) << (lsb_time_len * 8)) | lsbs;
+	res->is_falling = time & 1;
+	time >>= 1;
+	res->fine = time & ((1 << it->finetime_len) - 1);
+	time >>= it->finetime_len;
+	int32_t diff = time;
+	it->last_time = it->is_first ? diff : it->last_time - diff;
+	res->coarse = it->last_time;
+	res->channel = it->cur_chan;
 
-		it->is_first = is_last; // if this is the last time from the current channel, the next time will be the first one from the next channel...
+	it->is_first = is_last; // if this is the last time from the current channel, the next time will be the first one from the next channel...
 
-		if (is_last) {
-			++it->cur_chan;
-		}
+	if (is_last) {
+		++it->cur_chan;
+	}
 
-		return 1;
-	} while (1);
+	return 1;
 }
-
